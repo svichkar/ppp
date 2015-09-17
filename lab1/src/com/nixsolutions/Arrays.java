40d5f766a0522ed74e85ad6d2439ec2fe3c97732
evgeny_logovskoy <Dktfb94k>
2015-09-17 19:18:21 +0300

    Updated Arrays class

--- lab1/src/com/nixsolutions/Arrays.java
+++ lab1/src/com/nixsolutions/Arrays.java
@@ -2,33 +2,57 @@ package com.nixsolutions;
 
 import java.util.Random;
 
+
 public class Arrays {
 
 	//static int[] example = {157,545,8787,14,0,15,698,48,544,78};
-	private static int [] exs;
+	public static long[][] results = new long[20][2];
 	
 	
 	public static void main(String[] args) {
 
-		for(int i =0; i<20;i++){
+		for(int i = 0; i<20;i++){
 			int[] arr = new int[10000];
 			for(int j =0 ; j< arr.length;j++)
 			{
+				
 				Random rd = new Random();
 				int maximum = 100;
 				int minimum = -100;
 				int range = maximum - minimum + 1;
-				
-				int randomNum =  rd.nextInt(range) + minimum;
-				
+				int  randomNum =  rd.nextInt(range) + minimum;
 				arr[j] = randomNum;
+				
+			}
+			
+			long startTime= System.currentTimeMillis();
+			java.util.Arrays.sort(arr.clone());
+				
+				
+			long timeForCurrentIteration= System.currentTimeMillis() - startTime;
+			
+			results[i][0] = timeForCurrentIteration;
+			startTime= System.currentTimeMillis();
+			sort(arr.clone());	
+			timeForCurrentIteration= System.currentTimeMillis() - startTime;
+			results[i][1] = timeForCurrentIteration;
+		}
+		long avrerageTimeContainer[] = new long[2];
+		for(int i = 0; i < 2; i++){
+			    for(int j =0 ; j < 20; j ++){
+			    	avrerageTimeContainer[i] +=results[j][i];
+				
 			}
 			
 		}
+		System.out.println("Average time for Array.sort: "+ avrerageTimeContainer[0]/20 + " ms");
+		System.out.println("Average time for Bubble sorting method: "+ avrerageTimeContainer[1]/20 + " ms");
+		
+		System.out.println();
+		
 	}
-	public static int[] sort(int[] input ){
-		int[] arr  = new int[input.length];
-		arr = input;
+	
+	public static void sort(int[] arr ){
 		 for (int i = 0; i < arr.length; i++) {
 	            for (int j = 0; j < arr.length - i - 1; j++) {
 	                if (arr[j] > arr[j + 1]) {
@@ -38,8 +62,7 @@ public class Arrays {
 	            }
 	        }
 	    }
-		
-		return arr;
+			
 		
 	}
 

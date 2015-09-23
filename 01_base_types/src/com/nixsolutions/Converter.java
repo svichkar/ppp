276a7de3d100a8db73771154cbef4ceea5f37f4d
evgeny_logovskoy <Dktfb94k>
2015-09-23 17:53:25 +0300

    Upd

---
+++ 01_base_types/src/com/nixsolutions/Converter.java
@@ -0,0 +1,28 @@
+package com.nixsolutions;
+
+import java.io.*;
+import java.math.BigDecimal;
+
+public class Converter {
+
+  public void converter() {
+
+    System.out.println("Enter number to convertion here : ");
+    String inputString = null;
+    try {
+      BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+      inputString = bufferRead.readLine();
+
+    } catch (Exception e) {
+      e.printStackTrace();
+
+    }
+
+    BigDecimal bd = new BigDecimal(inputString);
+    System.out.println(bd.doubleValue());
+
+
+
+  }
+
+}

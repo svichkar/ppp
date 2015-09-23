de73911a209fe29ec81256e92fe528bf47822931
paryshev.a <paryshev.anton@nixsolutions.com>
2015-09-23 13:39:29 +0300

    update style

--- Lab1/src/com/nixsolutions/Task2.java
+++ Lab1/src/com/nixsolutions/Task2.java
@@ -5,19 +5,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Task2 {
-	public static void main(String[] args) {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		String inputString = "";
-		try {
-			inputString = reader.readLine().toString();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		if (inputString.toLowerCase().contains("e")){
-			System.out.format("%.10f", Double.valueOf(inputString));
-		}
-		else {
-			System.out.format("%.5E", Double.valueOf(inputString));
-		}
-	}
+  public static void main(String[] args) {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    String inputString = "";
+    try {
+      inputString = reader.readLine().toString();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    if (inputString.toLowerCase().contains("e")) {
+      System.out.format("%.10f", Double.valueOf(inputString));
+    } else {
+      System.out.format("%.5E", Double.valueOf(inputString));
+    }
+  }
 }

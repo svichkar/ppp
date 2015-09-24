705a9f489d4aec4045efa946cd29a99fb169dcc3
paryshev.a <paryshev.anton@nixsolutions.com>
2015-09-24 18:32:20 +0300

    upd

---
+++ 02_oop/src/com/nixsolutions/Bus.java
@@ -0,0 +1,19 @@
+package com.nixsolutions;
+
+/**
+ * Extends Transport class for Bus
+ */
+public class Bus extends Transport {
+  public Bus() {
+    setCapacity(40);
+    setDistance(0);
+  }
+
+  /**
+   * Implementation of method to add distance passed by one step by bus
+   */
+  @Override
+  public void addStepDistance() {
+    setDistance(getDistance() + 9);
+  }
+}

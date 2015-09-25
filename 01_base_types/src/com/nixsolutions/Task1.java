02a9486e8a4f82ea423e7f80040dbca351015e75
paryshev.a <paryshev.anton@nixsolutions.com>
2015-09-25 10:52:17 +0300

    upd

--- 01_base_types/src/com/nixsolutions/Task1.java
+++ 01_base_types/src/com/nixsolutions/Task1.java
@@ -10,28 +10,28 @@ public class Task1 {
     public static void countSortingTime() {
 	long[][] sortingTime = new long[2][20];
 	Random randomNumber = new Random();
-	int min = -100;
-	int max = 100;
-	long start = 0;
-	long end = 0;
+	int min = -100; // min value for Random
+	int max = 100; // max value for Random
+	long start = 0; // start time of sorting
+	long end = 0; // end time of sorting
+	long totalTimeBubbleSort = 0; // total time of sorting for bubble method
+	long totalTimeJavaUtilSort = 0; // total time of sorting for method from
+					// java util
 	for (int i = 0; i < 20; i++) {
 	    int[] initArray = new int[10000];
 	    for (int j = 0; j < 10000; j++) {
 		initArray[j] = randomNumber.nextInt(max - min) + min;
 	    }
 	    start = System.nanoTime();
-	    Arrays.sort(initArray);
+	    Arrays.sort(initArray); // sort array using bubble method
 	    end = System.nanoTime();
 	    sortingTime[0][i] = end - start;
 
 	    start = System.nanoTime();
-	    java.util.Arrays.sort(initArray);
+	    java.util.Arrays.sort(initArray); // sort array using method from
+					      // java util
 	    end = System.nanoTime();
 	    sortingTime[1][i] = end - start;
-	}
-	long totalTimeBubbleSort = 0;
-	long totalTimeJavaUtilSort = 0;
-	for (int i = 0; i < 20; i++) {
 	    totalTimeBubbleSort = totalTimeBubbleSort + sortingTime[0][i];
 	    totalTimeJavaUtilSort = totalTimeJavaUtilSort + sortingTime[1][i];
 	}
--- 01_base_types/src/com/nixsolutions/Task2.java
+++ 01_base_types/src/com/nixsolutions/Task2.java
@@ -9,14 +9,20 @@ public class Task2 {
 	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 	String inputString = "";
 	try {
-	    inputString = reader.readLine().toString();
+	    inputString = reader.readLine().toString(); // read typed value
 	} catch (IOException e) {
 	    e.printStackTrace();
 	}
 	if (inputString.toLowerCase().contains("e")) {
-	    System.out.format("%.10f", Double.valueOf(inputString));
+	    System.out.format("%.10f", Double.valueOf(inputString)); // print
+								     // value in
+								     // float
+								     // format
 	} else {
-	    System.out.format("%.5E", Double.valueOf(inputString));
+	    System.out.format("%.5E", Double.valueOf(inputString)); // print
+								    // value in
+								    // exponential
+								    // format
 	}
     }
 }

1dfe2d2c5f873f737c5b2ba3202e3a59a6420a02
paryshev.a <paryshev.anton@nixsolutions.com>
2015-09-28 16:14:37 +0300

    upd

---
+++ 04_dates/src/com/nixsolutions/DateTask5.java
@@ -0,0 +1,39 @@
+package com.nixsolutions;
+
+import java.util.Locale;
+import java.util.Date;
+import java.text.DateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTask5 {
+    public static void main(String[] args) {
+
+	// Java 7
+	Date currentDate = new Date();
+	DateFormat df = DateFormat.getDateInstance(DateFormat.FULL, Locale.CANADA);
+	System.out.println(df.format(currentDate));
+	df = DateFormat.getDateInstance(DateFormat.FULL, Locale.GERMANY);
+	System.out.println(df.format(currentDate));
+	Locale locale = new Locale("en", "PK");
+	df = DateFormat.getDateInstance(DateFormat.FULL, locale);
+	System.out.println(df.format(currentDate));
+	locale = new Locale("vi", "VN");
+	df = DateFormat.getDateInstance(DateFormat.FULL, locale);
+	System.out.println(df.format(currentDate));
+	
+	// Java 8
+	DateTimeFormatter dtf = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+	LocalDateTime curDate = LocalDateTime.now();
+	dtf.withLocale(Locale.CANADA);
+	System.out.println(dtf.format(curDate));
+	dtf.withLocale(Locale.GERMANY);
+	System.out.println(dtf.format(curDate));
+	locale = new Locale("en", "PK");
+	dtf.withLocale(locale);
+	System.out.println(dtf.format(curDate));
+	locale = new Locale("vi", "VN");
+	dtf.withLocale(locale);
+	System.out.println(dtf.format(curDate));
+    }
+}

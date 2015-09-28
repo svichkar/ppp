5d306476b5eb7a4aef375c95b3847c5b9231fa24
evgeny_logovskoy@gmail.com <Dktfb94k>
2015-09-28 20:19:24 +0300

    style

--- 04_dates/src/com/nixsolutions/StartApp.java
+++ 04_dates/src/com/nixsolutions/StartApp.java
@@ -3,12 +3,12 @@ package com.nixsolutions;
 public class StartApp {
 
     public static void main(String[] args) {
-	 DatesProcessor dp = new DatesProcessor();
-	      dp.mondayFinder();
-	      dp.showDateInDifferenLocales();
-	      dp.dateCalc();
-	      dp.isDateFirdayThirteenth();
-	      dp.getLenghtOfEachMonthForParticularYear();
+	DatesProcessor dp = new DatesProcessor();
+	dp.mondayFinder();
+	dp.showDateInDifferenLocales();
+	dp.dateCalc();
+	dp.isDateFirdayThirteenth();
+	dp.getLenghtOfEachMonthForParticularYear();
 
     }
 

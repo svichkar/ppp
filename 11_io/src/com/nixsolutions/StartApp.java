3d84ae4ef38c3e75da6dab8edb3084253d3904fd
evgeny_logovskoy@gmail.com <Dktfb94k>
2015-10-03 18:01:28 +0300

    add

---
+++ 11_io/src/com/nixsolutions/StartApp.java
@@ -0,0 +1,19 @@
+package com.nixsolutions;
+
+import java.nio.file.Path;
+
+public class StartApp {
+
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+    public static boolean copy(Path folderPath, Path destination){
+	
+	
+	
+	
+	return false;
+    }
+
+}

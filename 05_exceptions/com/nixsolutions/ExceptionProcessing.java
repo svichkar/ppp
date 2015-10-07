742657164a34a30c5494de7e2d88324dba70cf7f
unknown <kulishov@mkulishov.nixsolutions.com>
2015-10-07 10:06:41 +0300

    fix

--- 05_exceptions/com/nixsolutions/ExceptionProcessing.java
+++ 05_exceptions/com/nixsolutions/ExceptionProcessing.java
@@ -4,8 +4,12 @@ public class ExceptionProcessing {
 
 	public static void main(String[] args) {
 		CustomSave cSave = new CustomSave();
-		cSave.save("test", "D:\\test.txt");
-		cSave.save("", "D:\\test.txt");
-		cSave.save("test3", "D:\\test.txt");
+		try {
+			cSave.save("test", "D:\\test.txt");
+			cSave.save("", "D:\\test.txt");
+			cSave.save("test3", "D:\\test.txt");
+		} catch (CustomException ex) {
+			System.out.println("We catched our custom exception in 'main' method.");
+		}
 	}
 }

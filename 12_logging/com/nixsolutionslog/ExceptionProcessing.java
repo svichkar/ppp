325d370cc787d4e3690ef1e019de5bf0e1be49ec
unknown <kulishov@mkulishov.nixsolutions.com>
2015-10-14 18:10:09 +0300

    logging failure

--- 12_logging/com/nixsolutionslog/ExceptionProcessing.java
+++ 12_logging/com/nixsolutionslog/ExceptionProcessing.java
@@ -11,10 +11,12 @@ public class ExceptionProcessing {
 	public static void main(String[] args) {
 		CustomSave cSave = new CustomSave();
 		try {
+			LOGGER.entry();
 			cSave.save("test", "F:\\test.txt");
 			LOGGER.debug("Coming to exception point.");
-			cSave.save("", "D:\\test.txt");
+			//cSave.save("", "D:\\test.txt");
 			cSave.save("test3", "D:\\test.txt");
+			LOGGER.exit("Flow tracing.");
 		} catch (CustomException ex) {
 			LOGGER.error("We catched our custom exception in 'main' method.", ex);
 		}

8ad2b48dd55f40d39ec45e61edf42d5a5cea1766
paryshev.a <paryshev.anton@nixsolutions.com>
2015-10-14 10:44:01 +0300

    upd

--- 09_threads/src/com/nixsolutions/Consumer.java
+++ 09_threads/src/com/nixsolutions/Consumer.java
@@ -1,6 +1,5 @@
 package com.nixsolutions;
 
-import java.util.Iterator;
 import java.util.List;
 
 public class Consumer implements Runnable {
@@ -14,31 +13,20 @@ public class Consumer implements Runnable {
 
     @Override
     public void run() {
-	for (int i = 0; i < 5; i++) {
-	    if (randomNumbersList.size() == 0) {
-		try {
-		    Thread.sleep(250);
-		} catch (InterruptedException e) {
-		    e.printStackTrace();
+	int iter = 0;
+	while (iter < 100) {
+	    if (iter < randomNumbersList.size()) {
+		int nextElement = randomNumbersList.get(iter);
+		if (isEven) {
+		    if (nextElement % 2 == 0) {
+			System.out.println("Even: " + nextElement);
+		    }
+		} else {
+		    if (nextElement % 2 == 1) {
+			System.out.println("Odd: " + nextElement);
+		    }
 		}
-	    } else {
-		break;
-	    }
-	    if (randomNumbersList.size() == 0) {
-		throw new RuntimeException("No elements in queue for a long time");
-	    }
-	}
-	Iterator<Integer> iter = randomNumbersList.iterator();
-	while (iter.hasNext()) {
-	    int nextElement = iter.next();
-	    if (isEven) {
-		if (nextElement % 2 == 0) {
-		    System.out.println("Even: " + nextElement);
-		}
-	    } else {
-		if (nextElement % 2 == 1) {
-		    System.out.println("Odd: " + nextElement);
-		}
+		iter++;
 	    }
 	}
     }

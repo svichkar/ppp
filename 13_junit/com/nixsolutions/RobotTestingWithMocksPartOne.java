2c283d86512f8c84f4a1eef57a8b6ea76bdc6f84
unknown <kulishov@mkulishov.nixsolutions.com>
2015-10-16 18:55:22 +0300

    transition

--- 13_junit/com/nixsolutions/RobotTestingWithMocksPartOne.java
+++ 13_junit/com/nixsolutions/RobotTestingWithMocksPartOne.java
@@ -1,16 +1,38 @@
 package com.nixsolutions;
 
-import java.io.File;
+import java.io.IOException;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import static org.mockito.Mockito.*;
+import static org.mockito.BDDMockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RobotTestingWithMocksPartOne {
-	@Mock private File file;
-	@InjectMocks private Program program;
-	
-	
+	@Mock
+	private Robot robot;
+	@InjectMocks
+	private Program program;
+
+	@Before
+	public void initialize() throws IOException {
+		program = new Program("D:\\");
+	}
+
+	@Test
+	public void t() throws IOException {
+		// given
+		String command = "f";
+		when(robot.getCoordX()).thenReturn(2);
+		// when
+		program.execute(command);
+		// then
+		//verify(robot).stepForward();
+		Assert.assertEquals(1, program.getRobot().getCoordX());
+	}
 }

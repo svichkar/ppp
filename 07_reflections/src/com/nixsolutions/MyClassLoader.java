da0caf582bf6c16cbb94c0f181ba62f681b513a1
evgeny_logovskoy@gmail.com <Dktfb94k>
2015-10-22 16:53:55 +0300

    upd

--- 07_reflections/src/com/nixsolutions/MyClassLoader.java
+++ 07_reflections/src/com/nixsolutions/MyClassLoader.java
@@ -6,43 +6,45 @@ import java.io.IOException;
 
 public class MyClassLoader extends ClassLoader implements PathClassLoader {
 
-    private String path = "";
+	private String path = "";
 
-    @Override
-    public void setPath(String dir) {
-	path = dir;
+	@Override
+	public void setPath(String dir) {
+		path = dir;
 
-    }
-
-    @Override
-    public String getPath() {
-	// TODO Auto-generated method stub
-	return path;
-    }
-
-    public Class getInstanceByPath() {
-	FileInputStream fs = null;
-	try {
-	    File file = new File(path);
-	    fs = new FileInputStream(file);
-	    byte[] arr = new byte[(int) file.length()];
-	    fs.read(arr);
-
-	    return defineClass(null, arr, 0, arr.length);
-
-	} catch (IOException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	} finally {
-	    try {
-		fs.close();
-	    } catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	    }
 	}
 
-	return null;
-    }
+	@Override
+	public String getPath() {
+		return path;
+	}
+
+	public Class getInstanceByName(String className) {
+		FileInputStream fs = null;
+		try {
+			File file = new File(path + getAbsoutePath(className));
+			fs = new FileInputStream(file);
+			byte[] arr = new byte[(int) file.length()];
+			fs.read(arr);
+
+			return defineClass(null, arr, 0, arr.length);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				fs.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return null;
+	}
+	public String getAbsoutePath(String input) {
+		return input.replace('.', '/') + ".class";
+	}
 
 }
--- 07_reflections/src/com/nixsolutions/StartApp.java
+++ 07_reflections/src/com/nixsolutions/StartApp.java
@@ -2,30 +2,30 @@ package com.nixsolutions;
 
 public class StartApp {
 
-    @ToString
-    public int exp = 123;
+	@ToString
+	public int exp = 123;
 
-    @ToString
-    public String su = "BLA BLA";
+	@ToString
+	public String su = "BLA BLA";
 
-    @ToString
-    public Long longg = 58974L;
+	@ToString
+	public Long longg = 58974L;
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-	// Task 1
-	Annotator a = new Annotator(StartApp.class);
+		// Task 1
+		Annotator a = new Annotator(StartApp.class);
 
-	// Task 2
-	MyClassLoader cl = new MyClassLoader();
-	cl.setPath("D:/Java/JavaDepartment/Project/logovskoy/javappp/02_OOP/bin/src/com/nixsolutions/Figure.class");
-	Class c = cl.getInstanceByPath();
-	cl.setPath("D:/Java/JavaDepartment/Project/logovskoy/javappp/02_OOP/bin/src/com/nixsolutions/Circle.class");
-	Class cd = cl.getInstanceByPath();
+		// Task 2
+		MyClassLoader cl = new MyClassLoader();
+		cl.setPath(
+				"D:/Java/JavaDepartment/Project/logovskoy/javappp/02_OOP/bin/src/");
+		Class c = cl.getInstanceByName("com.nixsolutions.Figure");
+		Class cd = cl.getInstanceByName("com.nixsolutions.Circle");
 
-	System.out.println(c);
-	System.out.println(cd);
+		System.out.println(c);
+		System.out.println(cd);
 
-    }
+	}
 
 }

925a81a9f21e2f5f623ba6fa9b7cf72fd22a0715
ivan_usanin <ivan.usanin@gmail.com>
2015-10-26 00:29:10 +0200

    Lab13 mocks

---
+++ Lab13_mocks/src/com/nixsolutions/Lab13.java
@@ -0,0 +1,12 @@
+package com.nixsolutions;
+
+public class Lab13 {
+
+	public static void main(String[] args) throws Exception {
+		Robot robot = new Robot();
+		Program program = new Program(robot);
+		program.launchRobot("lffrflfrrfff");
+	}
+
+	
+}

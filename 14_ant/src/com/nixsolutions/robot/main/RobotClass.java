cf13180678262fe49e66d89f35e59de6e1d0467c
mixeyes <mixeyes82@gmail.com>
2015-10-26 16:05:47 +0200

    add 14_ant

---
+++ 14_ant/src/com/nixsolutions/robot/main/RobotClass.java
@@ -0,0 +1,100 @@
+/**
+ * 
+ */
+package com.nixsolutions.robot.main;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.file.Files;
+
+/**
+ * @author mixeyes
+ *
+ */
+public class RobotClass {
+	private Integer coordX;
+	public Integer getCoordX() {
+		return coordX;
+	}
+
+	public void setCoordX(Integer coordX) {
+		this.coordX = coordX;
+	}
+
+	public Integer getCoordY() {
+		return coordY;
+	}
+
+	public void setCoordY(Integer coordY) {
+		this.coordY = coordY;
+	}
+
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+
+	private Integer coordY;
+	private Course course;
+	private RobotLogger robotMoves;
+
+	/**
+	 * @param coordX
+	 * @param coordY
+	 * @param course
+	 */
+	public RobotClass() {
+		this.coordX = 0;
+		this.coordY = 0;
+		this.course = Course.RIGHT;
+		robotMoves = new RobotLogger();
+	}
+
+	public RobotClass(RobotLogger logger) {
+		this.coordX = 0;
+		this.coordY = 0;
+		this.course = Course.RIGHT;
+		robotMoves = logger;
+
+	}
+
+	public void turnLeft() throws IOException {
+		this.course = Course.getCourseByInt((this.course.getIntValue() + 1) % 4);
+		robotMoves.writeToLog(this);
+	}
+
+	public void turnRight() throws IOException {
+		this.course = Course.getCourseByInt((this.course.getIntValue() - 1) % 4);
+		robotMoves.writeToLog(this);
+	}
+
+	public void stepForward() throws IOException {
+		switch (course) {
+		case UP:
+			coordY += 1;
+			break;
+		case DOWN:
+			coordY -= 1;
+			break;
+		case RIGHT:
+			coordX += 1;
+			break;
+		case LEFT:
+			coordX -= 1;
+		default:
+			break;
+		}
+		robotMoves.writeToLog(this);
+	}
+
+
+}
---
+++ 14_ant/src/com/nixsolutions/robot/main/RobotLogger.java
@@ -0,0 +1,49 @@
+package com.nixsolutions.robot.main;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class RobotLogger {
+	private File logFile;
+
+	public RobotLogger() {
+		String curDir = System.getProperty("user.dir");
+		logFile = new File(curDir + "\\logs");
+		if (!logFile.exists())
+			logFile.mkdir();
+		logFile = new File(logFile, "robot.log");
+		if (logFile.exists())
+			logFile.delete();
+		// TODO Auto-generated constructor stub
+	}
+
+	public RobotLogger(String filePath) {
+		logFile = new File(filePath);
+		if (logFile.isDirectory())
+			logFile = new File(logFile, "robot.log");
+		if (logFile.exists())
+			logFile.delete();
+		// TODO Auto-generated constructor stub
+	}
+
+	public void writeToLog(RobotClass robot) throws IOException {
+		String logger = "";
+		if (!logFile.exists()) {
+			logFile.isDirectory();
+			logFile.createNewFile();
+		}
+		for (String line : Files.readAllLines(logFile.toPath())) {
+			logger += line + "\n";
+		}
+		FileWriter fw = new FileWriter(logFile);
+		BufferedWriter bw = new BufferedWriter(fw);
+
+		bw.write(logger
+				+ String.format("Robot position X: %1$s; Y: %2$s; Course: %3$s", robot.getCoordX(), robot.getCoordY(), robot.getCourse().toString()));
+		bw.flush();
+		bw.close();
+	}
+}

7000f62f7d6f5204ecbd58c3e4e5d5f37fbcb218
unknown <maxb@borkivets.nixsolutions.com>
2015-10-26 13:51:16 +0200

    delete from class MyLogger.java

--- 12_logging/com/nixsolutions/MyLogging.java
+++
@@ -1,8 +0,0 @@
-package com.nixsolutions;
-
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-
-public class MyLogging {
-	public static final Logger LOG = LogManager.getLogger("MyLogger");
-}

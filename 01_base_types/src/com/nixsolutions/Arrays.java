33ac141a575bd18d365032f5df3dd5efaf7a3993
cherednyk <kate.cherednik@gmail.com>
2015-11-13 18:16:56 +0200

    laba1 corrected

--- 01_base_types/src/com/nixsolutions/Arrays.java
+++ 01_base_types/src/com/nixsolutions/Arrays.java
@@ -1,29 +1,23 @@
 package com.nixsolutions;
 
-
 public class Arrays {
-	
-		
-	public static int[] sort(int[] arr){
+
+	public static int[] sort(int[] arr) {
 		int[] sortArr;
 		sortArr = new int[arr.length];
 		System.arraycopy(arr, 0, sortArr, 0, arr.length);
-		 for(int i = sortArr.length-1 ; i > 0 ; i--){
-	        for(int j = 0 ; j < i ; j++){
-	                if( sortArr[j] > sortArr[j+1] ){
-	                int tmp = sortArr[j];
-	                sortArr[j] = sortArr[j+1];
-	                sortArr[j+1] = tmp;
-	            }
-	        }
-	    }
-	      
-	      return sortArr;
-	}
-	
-	
-	
-	}
-	
 
+		for (int i = sortArr.length - 1; i > 0; i--) {
+			for (int j = 0; j < i; j++) {
+				if (sortArr[j] > sortArr[j + 1]) {
+					int tmp = sortArr[j];
+					sortArr[j] = sortArr[j + 1];
+					sortArr[j + 1] = tmp;
+				}
+			}
+		}
 
+		return sortArr;
+	}
+
+}
--- 01_base_types/src/com/nixsolutions/Simple.java
+++ 01_base_types/src/com/nixsolutions/Simple.java
@@ -5,82 +5,54 @@ import java.util.stream.IntStream;
 import java.util.Arrays;
 
 public class Simple {
-	
-	//private static long time;
 
-		
-		public static void main(String[] args) {
-						
-			Random rand = new Random();
-			
-			//com.nixsolutions.Arrays nameVariable = new com.nixsolutions.Arrays();
-			
-			int[] anArray;
-			anArray = new int[10000];
-			
-			long times;
-			
-			int timeEnd;
-			
-			int[][] timeSort;
-			timeSort = new int[2][20];
-			
-			for(int i=0; i<20; i++)
-			{
-				for(int j=0; j<10000; j++)
-				{
-			
-				anArray[j] = -100 + rand.nextInt(200); // Random between -100 and 100
-							
-				}
-				
-				//Sort1
-				
-				times = startBenchmark();				
-				com.nixsolutions.Arrays.sort(anArray);
-				timeEnd = stopBenchmark(times);
-				timeSort[0][i]=timeEnd;
-												
-				
-				//Sort2
-								
-				times = startBenchmark();
-				Arrays.sort(anArray);
-				timeEnd = stopBenchmark(times);
-				timeSort[1][i]=timeEnd;
-									
-			
-		}
-			
-			for(int i=0; i<timeSort.length; i++) {
-				  for(int j=0; j<timeSort[i].length; j++) {
-				    System.out.print(timeSort[i][j] + "\t");
-				  }
-				    System.out.println();
-			 		
+	public static void main(String[] args) {
+
+		Random rand = new Random();
+
+		int[] anArray;
+		anArray = new int[10000];
+
+		long timeStart;
+		long timeEnd;
+
+		int[][] timeSort;
+		timeSort = new int[2][20];
+
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < 10000; j++) {
+
+				anArray[j] = -100 + rand.nextInt(200); // Random between -100
+														// and 100
 			}
-			
-			
-			System.out.println(IntStream.of(timeSort[0]).average());
-			System.out.println(IntStream.of(timeSort[1]).average());
-			
-		}
-			
-		private static long startBenchmark() {
-			long timeStart;
+
+			// Sort1
+
 			timeStart = System.currentTimeMillis();
-			return timeStart;
-			
+			com.nixsolutions.Arrays.sort(anArray);
+			timeEnd = System.currentTimeMillis() - timeStart;
+			timeSort[0][i] = (int) timeEnd;
+
+			// Sort2
+
+			timeStart = System.currentTimeMillis();
+			Arrays.sort(anArray);
+			timeEnd = System.currentTimeMillis() - timeStart;
+			timeSort[1][i] = (int) timeEnd;
+
 		}
-		
-		private static int stopBenchmark(long start) {
-			long time;
-			time = System.currentTimeMillis() - start;
-			System.out.println("time="+time);
-			return (int)time;
-									
+
+		for (int i = 0; i < timeSort.length; i++) {
+			for (int j = 0; j < timeSort[i].length; j++) {
+				System.out.print(timeSort[i][j] + "\t");
+			}
+			System.out.println();
+
 		}
+
+		System.out.println(IntStream.of(timeSort[0]).average());
+		System.out.println(IntStream.of(timeSort[1]).average());
 
 	}
 
-
+}

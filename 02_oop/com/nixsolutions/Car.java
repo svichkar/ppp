7ba029079bdea67b8d0be3684ec62d74ae700a39
jax_on <jax.o.fly@gmail.com>
2015-11-13 17:21:22 +0200

    lab #2

--- 02_oop/com/nixsolutions/Car.java
+++ 02_oop/com/nixsolutions/Car.java
@@ -1,11 +1,27 @@
 package com.nixsolutions;
 
-public class Car extends Machine {
+/**
+ * an instance of this class represents a Car which able to move forward, left
+ * and right directions with speed equals to 11 points
+ * 
+ * @author kryzhanovskiy
+ *
+ */
+public class Car extends Vehicle implements Movable {
 
-	Car() {
-		this.setName("Car");
-		this.setSpeed(11);
+	@Override
+	public void move() {
+		this.setDistance(11);
+	}
 
+	@Override
+	public void turnLeft() {
+		this.setDistance(11);
+	}
+
+	@Override
+	public void turnRight() {
+		this.setDistance(11);
 	}
 
 }
--- 02_oop/com/nixsolutions/Main.java
+++ 02_oop/com/nixsolutions/Main.java
@@ -6,78 +6,60 @@ import java.util.Random;
 public class Main {
 
 	public static void main(String[] args) {
-for (int i = 0; i < 5; i++) {
-	System.out.println(random(1, 5));
-}
-		// создание массива рандомных транспортных средств размером в 10
-		// элементов.
-		Machine[] autopark = generateArr();
 
-		// далее 5 циклов движения, каждый цикл это 1 шаг влево или 1 шаг вправо
-		// или 1-5 шагов вперед.
+		// creation of an array with 10 vehicles 
+		Movable[] vehicles = generateArr();
+
+		//Execution of the 5 movement cycles
 		for (int i = 0; i < 5; i++) {
-		/*	switch (random(1, 3)) {
-			case 1:
-				for (Machine machine : autopark) {
-					machine.turnLeft();
-				}
-				break;
-			case 2:
-				for (Machine machine : autopark) {
-					machine.turnRight();
-				}
-				break;
-			case 3:
-				for (int y = 0; y < random(1, 5); y++) {
-					
-					for (Machine machine : autopark) {
-						machine.move();
-						System.out.println(y);
-					}
-					
-				}
-				break;
-			default:
-				break;
-			}
-		}*/
-			
-			for (int j = 0; j < autopark.length; j++) {
-				mooveCycle(autopark[j]);
+			for (int j = 0; j < vehicles.length; j++) {
+				mooveCycle(vehicles[j]);
 			}
 		}
-		// сортировка массива по пройденной дистанции.
-		Arrays.sort(autopark);
 
-		for (Machine machine : autopark) {
-			System.out.println(machine.getName() + " " + machine.getDistance());
+		// sort of the vehicles array by distance 
+		Arrays.sort(vehicles);
+
+		// output of the array in to the console
+		for (Movable machine : vehicles) {
+			System.out.println(machine.getClass().getSimpleName() + " " + ((Vehicle) machine).getDistance());
 		}
-		
-}
-	public static Machine[] generateArr() {
 
-		Machine[] autopark = new Machine[10];
-		for (int i = 0; i < autopark.length; i++) {
-
-			switch (random(1, 3)) {
-			case 1:
-				autopark[i] = new Car();
-				break;
-			case 2:
-				autopark[i] = new Bus();
-				break;
-			case 3:
-				autopark[i] = new Bicycle();
-				break;
-			default:
-				break;
-			}
-
-		}
-		return autopark;
 	}
 
-	public static void mooveCycle(Machine auto) {
+	/**
+	 * generates an array of 10 random vehicles: Car, Bus, Bicycle
+	 * 
+	 * @return an array of Movable[] vehicles
+	 */
+	public static Movable[] generateArr() {
+		Movable[] vehicles = new Movable[10];
+		for (int i = 0; i < vehicles.length; i++) {
+			switch (random(1, 3)) {
+			case 1:
+				vehicles[i] = new Car();
+				break;
+			case 2:
+				vehicles[i] = new Bus();
+				break;
+			case 3:
+				vehicles[i] = new Bicycle();
+				break;
+			default:
+				break;
+			}
+		}
+		return vehicles;
+	}
+
+	/**
+	 * executes randomly move(), turnLeft, turnRight() methods of a device with
+	 * implemented Movable interface
+	 * 
+	 * @param auto
+	 *            an object with implemented Movable interface
+	 */
+	public static void mooveCycle(Movable auto) {
 		switch (random(1, 3)) {
 		case 1:
 			auto.turnLeft();
@@ -95,10 +77,18 @@ for (int i = 0; i < 5; i++) {
 		}
 	}
 
+	/**
+	 * generates a random number within the given range
+	 * 
+	 * @param min
+	 *            minimal value of the range
+	 * @param max
+	 *            maximum value of the range
+	 * @return random int value within the given range
+	 */
 	public static int random(int min, int max) {
 		Random rand = new Random();
 		int randomNum = rand.nextInt((max - min) + 1) + min;
 		return randomNum;
-
 	}
 }

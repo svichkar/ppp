7ea3ece8a9e7f00ff3bd706c91b9fcd862180001
kozlovskij <s.kozlovskij@nixsolutions.com>
2015-11-13 17:27:13 +0200

    code for 5th lab

---
+++ 05_exceptions/com/nixsolutions/CustomSave.java
@@ -0,0 +1,39 @@
+package com.nixsolutions;
+
+import java.io.*;
+
+/**
+ * Created by kozlovskij on 11/13/2015.
+ */
+public class CustomSave implements exception.Save {
+    @Override
+    public void save(String text, String path) throws MyException {
+
+        File file = new File(path);
+
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                throw new MyException(e);
+            }
+        }
+
+        OutputStream outputStream = null;
+        try {
+            outputStream = new FileOutputStream(file, false);
+            outputStream.write(text.getBytes());
+            outputStream.flush();
+        } catch (IOException e) {
+            throw new MyException(e);
+        } finally {
+            try {
+                if (outputStream != null) {
+                    outputStream.close();
+                }
+            }  catch (IOException e) {
+                throw new MyException(e);
+            }
+        }
+    }
+}
---
+++ 05_exceptions/com/nixsolutions/Test.java
@@ -0,0 +1,19 @@
+package com.nixsolutions;
+
+import java.io.IOException;
+
+/**
+ * Created by kozlovskij on 11/13/2015.
+ */
+public class Test {
+    public static void main(String[] args) {
+        CustomSave test = new CustomSave();
+        try {
+            test.save("test", "D:\\test.txt");
+            test.save("test1", "");
+            test.save("test2", "D:\\test.txt");
+        } catch (MyException e) {
+            System.out.printf("We caught exception: %s%n", e.toString());
+        }
+    }
+}

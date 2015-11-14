002cbd71b97cf9a88ed5202f4af5bbe6f1badc59
pixtart <pixtart@mail.ru>
2015-11-14 23:16:22 +0200

    labs 04_dates have been completed

--- 04_dates/src/com/nixsolutions/WorkWithDates.java
+++ 04_dates/src/com/nixsolutions/WorkWithDates.java
@@ -3,21 +3,39 @@ package com.nixsolutions;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.*;
+
+import java.time.*;
 
 public class WorkWithDates {
 
-    public static void main(String[] args) throws ParseException{
-	/*Ќаписать метод который дл€ указанной даты возвращает строку в которой написано сколько лет, мес€цев, дней прошло с этой даты. Ќапример У2 года, 1 мес€ц и 23 дн€Ф
-»спользу€ локаль дл€  анады, √ермании, ѕакистана и ¬ьетнама вывести текущую дату в полном формате использу€ Date-Time API Java 8 и возможности Java 7
-*/      System.out.println("Please, enter the date in format dd-MM-yyyy.");
-        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+    public static void main(String[] args){
+
+        DateFormat dateFormat;
+        Calendar calendar;
+        Date date = null;
+        boolean dateEntered = false;
         Scanner inputData = new Scanner(System.in);
-        String inputWholeDate = inputData.nextLine();
-        Calendar calendar = Calendar.getInstance();
-        calendar.setLenient(false);
-        dateFormat.setCalendar(calendar);
-        Date date = dateFormat.parse(inputWholeDate);
+        do {
+            System.out.println("Please, enter the date in format dd-MM-yyyy.");
+            dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+            String inputWholeDate = inputData.nextLine();
+            calendar = Calendar.getInstance();
+            calendar.setLenient(false);
+            dateFormat.setCalendar(calendar);
+            try{
+                date = dateFormat.parse(inputWholeDate);
+                dateEntered = true;
+            }catch (ParseException ex){
+                System.out.println("You entered date in invalid format! Press button 'q' to quit or any other to try " +
+                        "again");
+                String enteredValue = inputData.nextLine();
+                if(enteredValue.equals("q"))
+                    return;
+            }
+        } while(!dateEntered);
 
 
         System.out.println("\nWe're going to output each month capacity in entered year:");
@@ -31,9 +49,12 @@ public class WorkWithDates {
 
         System.out.println("\nCheck duration of time between entered and current date: ");
         timeAgoSince(date);
+
+        System.out.println("\n Print current date with diff locale using Java7 and Java8: \n");
+        outCurrentDateForDiffLocals();
     }
 
-    //Ќаписать метод который дл€ указанного года выводит длину каждого мес€ца.
+    //Duration of each month in input year
     public static void outputMonthLength(int year){
         Calendar calendar = Calendar.getInstance();
         calendar.setLenient(false);
@@ -65,6 +86,7 @@ public class WorkWithDates {
         System.out.println(out);
     }
 
+    //Method that check if entered date is Friday, 13n
     public static boolean isDateFridayThirteen(Date date){
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
@@ -77,11 +99,8 @@ public class WorkWithDates {
         return isFr13;
     }
 
-
-    //Ќаписать метод который дл€ указанной даты возвращает строку в которой написано сколько лет, мес€цев, дней прошло с этой даты. Ќапример У2 года, 1 мес€ц и 23 дн€Ф
-
-    public static String timeAgoSince(Date dateOld)
-    {
+    //Method that output duration between input and current date
+    public static String timeAgoSince(Date dateOld){
         Calendar currentDate = Calendar.getInstance();
         currentDate.setTime(new Date());
 
@@ -112,4 +131,31 @@ public class WorkWithDates {
         System.out.println(differencePhrase +" is passed since old  date!");
         return differencePhrase;
     }
+
+    // Print date using Java 8 and Java 7 in full format for different locals
+    public static void outCurrentDateForDiffLocals()
+    {
+        Locale localePakistan = new Locale("en", "PK"); //we use English because it is one of official pakistan's language
+        Locale localeVietnam = new Locale("vi", "VN");
+
+        //using Java 8
+        LocalDate locDate = LocalDate.now();
+        DateTimeFormatter tf = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL);
+
+        System.out.println("We will out date for different Locals in next rows using Java 8:\n");
+        System.out.println("Canada local: " + locDate.format(tf.withLocale(Locale.CANADA)));
+        System.out.println("Germany local: " + locDate.format(tf.withLocale(Locale.GERMANY)));
+        System.out.println("Pakistan local: " + locDate.format(tf.withLocale(localePakistan)));
+        System.out.println("Vietnam local: " + locDate.format(tf.withLocale(localeVietnam)) + "\n");
+
+        //using Java 7
+        Date currentDate = new Date();
+        System.out.println("We will out date for different Locals in next rows using Java 7:\n");
+        System.out.println("Canada local: " + DateFormat.getDateInstance(DateFormat.FULL, Locale.CANADA).format(currentDate));
+        System.out.println("Germany local: " + DateFormat.getDateInstance(DateFormat.FULL, Locale.GERMANY).format(currentDate));
+        System.out.println("Pakistan local: " + DateFormat.getDateInstance(DateFormat.FULL, localePakistan).format(currentDate));
+        System.out.println("Vietnam local: " + DateFormat.getDateInstance(DateFormat.FULL, localeVietnam).format(currentDate));
+
+    }
+
 }

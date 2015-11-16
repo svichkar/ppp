be0636766ee51331696b6731d6685ffa0a434d49
mednorcom <mednorcom@mednor.nixsolutions.com>
2015-11-16 11:42:07 +0200

    -added .clone to make initial array unchanged -updated formatting

--- 01_base_types/BaseTypes/src/basetypes/BaseTypes1.java
+++ 01_base_types/BaseTypes/src/basetypes/BaseTypes1.java
@@ -9,14 +9,15 @@ import java.util.Random;
 
 /**
  *
- * @author mednorcom
- * Task1: Создать com.nixsolutions.Arrays со статическим методом sort который на вход будет получать
- * массив целых чисел (int). В методе реализовать сортировку методом пузырька. Исходный массив
- * не должен изменяться методом. Написать программу которая будет в цикле (20 раз) создать массив
- * размером 10000 из случайных целых чисел состоящих от -100 до 100, и запускать сортировку 
- * пузырьком и метод сортировки из класса java.util.Arrays. При каждой итерации замерять время 
- * выполнения сортировки каждым способом и результаты складывать в двумерный массив. После окончания
- * цикла вывести среднее для значений, полученных каждым методом сортировки. 
+ * @author mednorcom Task1: Создать com.nixsolutions.Arrays со статическим
+ * методом sort который на вход будет получать массив целых чисел (int). В
+ * методе реализовать сортировку методом пузырька. Исходный массив не должен
+ * изменяться методом. Написать программу которая будет в цикле (20 раз) создать
+ * массив размером 10000 из случайных целых чисел состоящих от -100 до 100, и
+ * запускать сортировку пузырьком и метод сортировки из класса java.util.Arrays.
+ * При каждой итерации замерять время выполнения сортировки каждым способом и
+ * результаты складывать в двумерный массив. После окончания цикла вывести
+ * среднее для значений, полученных каждым методом сортировки.
  */
 public class BaseTypes1 {
 
@@ -31,38 +32,38 @@ public class BaseTypes1 {
 
         while (iteration < iterationsCount) {
             //create random array
-            System.out.printf("%d of %d\n", iteration+1, iterationsCount);
+            System.out.printf("%d of %d\n", iteration + 1, iterationsCount);
             Random randInt = new Random();
             int[] integers = new int[arraySize];
             for (int i = 0; i < integers.length; i++) {
-                integers[i] = randInt.nextInt(200)-100;
+                integers[i] = randInt.nextInt(200) - 100;
             }
 
             long startime = System.nanoTime();
-            int[] integersSorted = 
-                    com.nixsolutions.Arrays.sort(integers); //perform custom sort   
+            int[] integersSorted
+                    = com.nixsolutions.Arrays.sort(integers); //perform custom sort   
             timeLog[0][iteration] = System.nanoTime() - startime;
             startime = System.nanoTime();
             java.util.Arrays.sort(integers); //perform default sort 
             timeLog[1][iteration] = System.nanoTime() - startime;
             iteration++;
-            
+
         }
         long avgCustomSortTime = 0;
         long avgDefaultSortTime = 0;
-        for (int i = 0; i < iterationsCount-1; i++) {
+        for (int i = 0; i < iterationsCount - 1; i++) {
             if (i == 0) {
                 avgCustomSortTime = timeLog[0][i];
                 avgDefaultSortTime = timeLog[1][i];
             } else {
-                avgCustomSortTime = (avgCustomSortTime*i+timeLog[0][i+1])/(i+1);
-                avgDefaultSortTime = (avgDefaultSortTime*i+timeLog[1][i+1])/(i+1);
+                avgCustomSortTime = (avgCustomSortTime * i + timeLog[0][i + 1]) / (i + 1);
+                avgDefaultSortTime = (avgDefaultSortTime * i + timeLog[1][i + 1]) / (i + 1);
             }
         }
-       
+
         System.out.printf("Custom (com.nixsolutions) sort takes %dns in average\n"
                 + "Default (java.utils) sort takes %dns in average\n",
-                avgCustomSortTime,avgDefaultSortTime);
+                avgCustomSortTime, avgDefaultSortTime);
 
     }
 
--- 01_base_types/BaseTypes/src/com/nixsolutions/Arrays.java
+++ 01_base_types/BaseTypes/src/com/nixsolutions/Arrays.java
@@ -7,27 +7,27 @@ package com.nixsolutions;
 
 /**
  *
- * @author mednorcom
- * Task1: Создать com.nixsolutions.Arrays со статическим методом sort который на вход будет получать
- * массив целых чисел (int). В методе реализовать сортировку методом пузырька. Исходный массив
- * не должен изменяться методом. Написать программу которая будет в цикле (20 раз) создать массив
- * размером 10000 из случайных целых чисел состоящих от -100 до 100, и запускать сортировку 
- * пузырьком и метод сортировки из класса java.util.Arrays. При каждой итерации замерять время 
- * выполнения сортировки каждым способом и результаты складывать в двумерный массив. После окончания
- * цикла вывести среднее для значений, полученных каждым методом сортировки. 
+ * @author mednorcom Task1: Создать com.nixsolutions.Arrays со статическим
+ * методом sort который на вход будет получать массив целых чисел (int). В
+ * методе реализовать сортировку методом пузырька. Исходный массив не должен
+ * изменяться методом. Написать программу которая будет в цикле (20 раз) создать
+ * массив размером 10000 из случайных целых чисел состоящих от -100 до 100, и
+ * запускать сортировку пузырьком и метод сортировки из класса java.util.Arrays.
+ * При каждой итерации замерять время выполнения сортировки каждым способом и
+ * результаты складывать в двумерный массив. После окончания цикла вывести
+ * среднее для значений, полученных каждым методом сортировки.
  */
 public class Arrays {
 
     public static int[] sort(int[] inArray) {
-        int[] outArray = inArray;
+        int[] outArray = inArray.clone();
         int j;
         boolean flag = true;   // set flag to true to begin first pass
         int temp;   //holding variable
         while (flag) {
             flag = false;    //set flag to false awaiting a possible swap
             for (j = 0; j < outArray.length - 1; j++) {
-                if (outArray[j] < outArray[j + 1]) // change to > for ascending sort
-                {
+                if (outArray[j] < outArray[j + 1]) { // change to > for ascending sort
                     temp = outArray[j];                //swap elements
                     outArray[j] = outArray[j + 1];
                     outArray[j + 1] = temp;

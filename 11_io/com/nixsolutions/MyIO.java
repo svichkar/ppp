d4261806612b4342b06a3775a30a0cc7e9d4612b
kozlovskij <s.kozlovskij@nixsolutions.com>
2015-11-19 00:00:44 +0200

    final version

--- 11_io/com/nixsolutions/MyIO.java
+++ 11_io/com/nixsolutions/MyIO.java
@@ -1,28 +1,44 @@
 package com.nixsolutions;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.regex.Pattern;
 
 /**
  * Created by kozlovskij on 11/18/2015.
  */
 public class MyIO {
-    public static void isDir(File file) {
-
-        if (file.isDirectory()) {
-            System.out.println(file.getAbsolutePath());
-            File[] files = file.listFiles();
-            for (int i = 0; i < files.length; i++) {
-                isDir(files[i]);
-            }
-        }
+    public static void isDir(File source, File dest) throws IOException {
+       if (source.isDirectory()) {
+           if (!dest.exists()) {
+               dest.mkdir();
+           }
+           File[] files = source.listFiles();
+           for (File f:files) {
+               isDir(f, new File(dest, f.getName()));
+           }
+       } else {
+           BufferedInputStream in = new BufferedInputStream(new FileInputStream(source));
+           BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(dest));
+           byte[] bytes = new byte[1024];
+           int len;
+           while ((len = in.read(bytes)) > 0) {
+               out.write(bytes, 0, len);
+           }
+           in.close();
+           out.close();
+       }
     }
 
     public static void main(String[] args) {
-        String InputDir = "D:\\IO";
-        File input = new File(InputDir);
+        String inputDir = "D:\\test";
+        String outputDir = "D:\\atest";
+        File in = new File(inputDir);
+        File out = new File(outputDir);
 
-        isDir(input);
+        try {
+            isDir(in,out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }

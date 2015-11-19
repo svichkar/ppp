9f4373907805ea4ec58468a0d856adcb0dbafac8
kulishov <maks.kulishov@nixsolutions.com>
2015-11-19 02:27:53 +0200

    jpa

--- 18_jpa_hibernate/hibernate_part_one/src/main/java/com/nixsolutions/entity/Car.java
+++ 18_jpa_hibernate/hibernate_part_one/src/main/java/com/nixsolutions/entity/Car.java
@@ -1,4 +1,4 @@
-package entities;
+package com.nixsolutions.entity;
 
 import java.io.Serializable;
 
@@ -6,6 +6,8 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
@@ -14,6 +16,7 @@ public class Car implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "car_id")
 	private int car_id;
 	@Column(name = "model", length = 100, nullable = false)
--- 18_jpa_hibernate/hibernate_part_one/src/main/java/com/nixsolutions/entity/Customer.java
+++ 18_jpa_hibernate/hibernate_part_one/src/main/java/com/nixsolutions/entity/Customer.java
@@ -1,9 +1,11 @@
-package entities;
+package com.nixsolutions.entity;
 
 import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -11,6 +13,7 @@ public class Customer implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "customer_id")
 	private int customer_id;
 	@Column(name = "first_name", length = 25, nullable = false)

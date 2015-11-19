d7ef7ff70cb8b8a19675eb4722aea7ed7a11d180
mednorcom <mednorcom@mednor.nixsolutions.com>
2015-11-19 17:01:51 +0200

    -removed  comments in russian

--- 01_base_types/BaseTypes/src/basetypes/BaseTypes2.java
+++ 01_base_types/BaseTypes/src/basetypes/BaseTypes2.java
@@ -10,20 +10,18 @@ import java.util.Scanner;
 /**
  *
  * @author mednorcom
- * Task2: Написать программу которая будет просить пользователя вводить числа с плавающей точкой и 
- * выводить на экран введенное значение в экспоненциальном виде, если оно было введено в нормальном 
- * виде и, наоборот, выводить в нормальном если число было введено в экспоненциальном виде
+ *
  */
 public class BaseTypes2 {
 
     public static void main(String[] args) {
 
-        System.out.printf("Please enter your number\n");       
-        String userInput = new Scanner(System.in).nextLine();      
-        
+        System.out.printf("Please enter your number\n");
+        String userInput = new Scanner(System.in).nextLine();
+
         if (userInput.matches("\\d+e.*\\d+\n")) {
             Double parsedValue = Double.parseDouble(userInput);
-            System.out.printf("Converted: %f\n",parsedValue);
+            System.out.printf("Converted: %f\n", parsedValue);
         } else if (userInput.matches("\\d+.*\\d*\n")) {
             Double parsedValue = Double.parseDouble(userInput);
             System.out.printf("Converted: %e\n", parsedValue);
@@ -31,6 +29,6 @@ public class BaseTypes2 {
             System.out.printf("Please enter decimal or exponential number"
                     + " like\na)3.14\nb)3e+10\n");
         }
-        
+
     }
 }

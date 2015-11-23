e65f56721858215a9366cc8104d745a088bcfaca
Evgeniy <evgeniusf@gmail.com>
2015-11-24 00:04:14 +0200

    gitignore

--- 06_collections/com/nixsolutions/collections/MainCollections.java
+++ 06_collections/com/nixsolutions/collections/MainCollections.java
@@ -1,51 +1,86 @@
 package com.nixsolutions.collections;
 
-import java.awt.List;
 import java.util.Iterator;
+import java.util.Scanner;
 
 public class MainCollections {
 
 	public static void main(String[] args) {
 
-		CustomCollection<String> strCollection = new CustomCollection<>();
+		CustomCollection<String> strCollection1 = new CustomCollection<>();
+		CustomCollection<Integer> intCollection1 = new CustomCollection<Integer>();
+
 		for (int i = 0; i < 5; i++) {
-			strCollection.add("fhsldhgls suka plohovataja pogoda " + i);
+			strCollection1.add("Collection's one stuff #" + i);
+			intCollection1.add(i);
 		}
 
-		CustomCollection<String> intCollection = new CustomCollection<>(4);
+		CustomCollection<String> strCollection2 = new CustomCollection<>(4);
+		CustomCollection<Integer> intCollection2 = new CustomCollection<Integer>(4);
 		for (int i = 0; i < 3; i++) {
-			intCollection.add("Someth " + i);
+			strCollection2.add("Second Collection's stuff " + i * 100);
+			intCollection2.add(i + 43);
 		}
-		strCollection.addAll(intCollection);
-		strCollection.addAll(strCollection);
-		/*
-		 * Object[] array = intCollection.toArray(); String[] y =
-		 * intCollection.toArray(new String[2]);
-		 */
-
-		if (strCollection.containsAll(intCollection)) {
-			System.out.println("All of them are here");
-		} else {
-			System.out.println("Not all of them are here");
+int i=0;
+		for (String str:strCollection1){
+			strCollection1.remove(str);
+			System.out.println("Ieration " + i+1);
+			i++;
 		}
-		intCollection.add("Sna,lsfkh");
-		intCollection.add("Someth 2");
-		if (strCollection.retainAll(intCollection)) {
-			System.out.println("All of them were removed");
-		} else {
-			System.out.println("Not all of them are here");
-		}
+		printCollection(strCollection1);
 		
-		if (strCollection.containsAll(intCollection)) {
-			System.out.println("All of them are here");
-		} else {
-			System.out.println("Not all of them are here");
-		}
-	  /*   Iterator iterator = strCollection.iterator();     
-	      while (iterator.hasNext()){
-	         System.out.print(iterator.next() + " ");  
-	      }
-	      System.out.println();*/
+		
+		
+		strCollection1.add("Just added string");
+		intCollection1.add(1002);
+
+		
+		
+		printCollection(strCollection1);
+		printCollection(intCollection1);
+
+		strCollection1.addAll(strCollection2);
+		intCollection1.addAll(intCollection2);
+
+		printCollection(strCollection1);
+		printCollection(intCollection1);
+
+		strCollection1.removeAll(strCollection2);
+		intCollection1.removeAll(intCollection2);
+
+		strCollection1.remove("Just added string");
+		intCollection1.remove(1);
+
+		printCollection(strCollection1);
+		printCollection(intCollection1);
+
+		strCollection1.addAll(strCollection2);
+		intCollection1.addAll(intCollection2);
+
+		printCollection(strCollection1);
+		printCollection(intCollection1);
+
+		strCollection1.retainAll(strCollection2);
+		intCollection1.retainAll(intCollection2);
+
+		printCollection(strCollection1);
+		printCollection(intCollection1);
+
+		strCollection1.removeAll(strCollection2);
+		intCollection1.removeAll(intCollection2);
+		System.out.println("Before removing 2nd collections");
+
+		printCollection(strCollection1);
+		printCollection(intCollection1);
+				
 	}
 
+	private static void printCollection(CustomCollection col) {
+		Iterator iterator = col.iterator();
+		while (iterator.hasNext()) {
+			System.out.print(iterator.next() + " \n");
+		}
+		System.out.println();
+	}
+	
 }

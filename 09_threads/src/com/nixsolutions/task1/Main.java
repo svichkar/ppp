b3d9d90214ec76ea288e2882392b30dfd714f75c
konstantin_svichkar <konstantin.svichkar@nixsolutions.com>
2015-11-24 22:32:56 +0200

    changed package name

--- 09_threads/src/com/nixsolutions/task1/Main.java
+++ 09_threads/src/com/nixsolutions/task1/Main.java
@@ -1,4 +1,4 @@
-package com.nixsolutions;
+package com.nixsolutions.task1;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
--- 09_threads/src/com/nixsolutions/task1/Producer.java
+++ 09_threads/src/com/nixsolutions/task1/Producer.java
@@ -1,4 +1,4 @@
-package com.nixsolutions;
+package com.nixsolutions.task1;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;

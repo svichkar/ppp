df077de467744bd9ff2e2f017941ad86a465a80d
jax_on <jax.o.fly@gmail.com>
2015-11-25 15:47:10 +0200

    lab 04

--- 04_dates/com/nixsolutions/DatesExp.java
+++ 04_dates/com/nixsolutions/DatesExp.java
@@ -15,7 +15,8 @@ public class DatesExp {
 
 	public static void main(String[] args) throws ParseException {
 
-		System.out.println("Please enter a date in the following format: dd MM yyyy");
+		System.out.println(
+				"Please enter a date in the following format: dd MM yyyy");
 		// set of User input
 		Scanner scan = new Scanner(System.in);
 		Calendar cal = Calendar.getInstance();
@@ -30,7 +31,8 @@ public class DatesExp {
 		System.out.println("Here are lenght of months in the given year");
 		lengthOfMonths(def, df);
 
-		System.out.println("\nHere are all the mondays of the month in the given year");
+		System.out.println(
+				"\nHere are all the mondays of the month in the given year");
 		getMondays(def, df);
 
 		System.out.println("\nIs the date Friday 13?");
@@ -49,9 +51,13 @@ public class DatesExp {
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(df);
 		cal.set(Calendar.MONTH, 0);
-		for (int i = 0; i < 11; i++) {
-			System.out.println("days in " + cal.getDisplayName(Calendar.MONTH, Calendar.LONG, def) + " "
-					+ cal.get(Calendar.YEAR) + ": " + cal.getActualMaximum(Calendar.DAY_OF_MONTH));
+		for (int i = 0; i < 12; i++) {
+			System.out
+					.println("days in "
+							+ cal.getDisplayName(Calendar.MONTH, Calendar.LONG,
+									def)
+							+ " " + cal.get(Calendar.YEAR) + ": "
+							+ cal.getActualMaximum(Calendar.DAY_OF_MONTH));
 			cal.roll(Calendar.MONTH, 1);
 		}
 	}
@@ -61,8 +67,11 @@ public class DatesExp {
 		cal.setTime(df);
 		cal.set(Calendar.DAY_OF_MONTH, 1);
 		for (int i = 1; i < cal.getActualMaximum(Calendar.DAY_OF_MONTH); i++) {
-			if (cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, def).equals("Monday")) {
-				System.out.println(DateFormat.getDateInstance(DateFormat.FULL, def).format(cal.getTime()));
+			if (cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, def)
+					.equals("Monday")) {
+				System.out.println(
+						DateFormat.getDateInstance(DateFormat.FULL, def)
+								.format(cal.getTime()));
 			}
 			cal.roll(Calendar.DAY_OF_MONTH, 1);
 		}
@@ -71,8 +80,8 @@ public class DatesExp {
 	public static void fridayThirteen(Locale def, Date df) {
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(df);
-		if (cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, def).equals("Friday")
-				&& cal.get(Calendar.DAY_OF_MONTH) == 13) {
+		if (cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, def)
+				.equals("Friday") && cal.get(Calendar.DAY_OF_MONTH) == 13) {
 			System.out.println("this is the day!!!");
 		} else {
 			System.out.println("this day is not Fri 13!!!");
@@ -94,8 +103,11 @@ public class DatesExp {
 		 */
 		long years = timeDiff / 31536000000L;
 		long months = (timeDiff - (years * 31536000000L)) / 2591999000L;
-		long days = (timeDiff - ((years * 31536000000L) + (months * 2592000000L))) / 86399000L;
-		System.out.println(years + " years " + months + " months " + days + " days");
+		long days = (timeDiff
+				- ((years * 31536000000L) + (months * 2592000000L)))
+				/ 86399000L;
+		System.out.println(
+				years + " years " + months + " months " + days + " days");
 
 	}
 
@@ -110,15 +122,26 @@ public class DatesExp {
 		// Java 7 approach
 		System.out.println("Java 7:");
 		for (int i = 0; i < locales.length; i++) {
-			System.out.println(locales[i].getDisplayCountry() + "= "
-					+ DateFormat.getDateInstance(DateFormat.FULL, locales[i]).format(new Date()));
+			System.out
+					.println(
+							locales[i].getDisplayCountry() + "= "
+									+ DateFormat
+											.getDateInstance(DateFormat.FULL,
+													locales[i])
+											.format(new Date()));
 		}
 
 		// Java 8 approach
 		System.out.println("Java 8:");
 		for (int i = 0; i < locales.length; i++) {
-			System.out.println(locales[i].getDisplayCountry() + "= " + LocalDateTime.now()
-					.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL).withLocale(locales[i])));
+			System.out
+					.println(
+							locales[i].getDisplayCountry() + "= "
+									+ LocalDateTime.now()
+											.format(DateTimeFormatter
+													.ofLocalizedDate(
+															FormatStyle.FULL)
+													.withLocale(locales[i])));
 		}
 	}
 

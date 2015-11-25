7afc0834b8b49e0c9b34bbc45bddf775ebc10a24
Evgeniy <evgeniusf@gmail.com>
2015-11-25 13:36:49 +0200

    Updated Exceptions

--- 05_exceptions/com/nixsolutions/exceptions/CustomException.java
+++ 05_exceptions/com/nixsolutions/exceptions/CustomException.java
@@ -2,7 +2,7 @@ package com.nixsolutions.exceptions;
 
 import java.io.IOException;
 
-public class CustomException extends Exception {
+public class CustomException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
--- 05_exceptions/com/nixsolutions/exceptions/SaveFile.java
+++ 05_exceptions/com/nixsolutions/exceptions/SaveFile.java
@@ -5,47 +5,41 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class SaveFile implements exception.Save {
-/**
- * Implementation of exception.Save
- */
+	/**
+	 * Implementation of exception.Save
+	 */
 	@Override
 	public void save(String strTextForSaving, String strPath) {
 		FileWriter writeFile = null;
+		if (strTextForSaving.isEmpty() || strPath.isEmpty()) {
+			throw new CustomException();
+		}
+		File fileToSave = new File(strPath);
 		try {
-			if (strTextForSaving.isEmpty() || strPath.isEmpty()) {
-				throw new CustomException();
-			}
-			File fileToSave = new File(strPath);
-			try {
-				if (fileToSave.exists()) {
+			if (fileToSave.exists()) {
 
-					System.out.println("File was rewritten");
-					writeFile = new FileWriter(fileToSave);
-					writeFile.write("");
+				System.out.println("File was rewritten");
+				writeFile = new FileWriter(fileToSave);
+				writeFile.write("");
 
-				} else {
+			} else {
 
-					System.out.println("File was created");
-					fileToSave.createNewFile();
-					writeFile = new FileWriter(fileToSave);
+				System.out.println("File was created");
+				fileToSave.createNewFile();
+				writeFile = new FileWriter(fileToSave);
 
-				}
-
-				writeFile.write(strTextForSaving);
-				writeFile.flush();
-
-			} catch (IOException e) {
-				throw new CustomException(e);
 			}
 
-		} catch (CustomException ex) {
-			throw new RuntimeException(ex);
+			writeFile.write(strTextForSaving);
+			writeFile.flush();
+
+		} catch (IOException e) {
+			throw new CustomException(e);
 		} finally {
 			try {
 				if (writeFile != null)
 					writeFile.close();
 			} catch (IOException e) {
-				e.printStackTrace();
 			}
 		}
 

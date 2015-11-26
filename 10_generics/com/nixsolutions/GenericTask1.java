bc11a49991f7955d70293c9d4953ee192c55975c
kozlovskij <serkozlovskij@yandex.ru>
2015-11-26 15:29:21 +0200

    added logging

--- 10_generics/com/nixsolutions/GenericTask1.java
+++ 10_generics/com/nixsolutions/GenericTask1.java
@@ -1,5 +1,8 @@
 package com.nixsolutions;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,15 +13,23 @@ import java.util.Set;
  * Created by Serko on 25.11.2015.
  */
 public class GenericTask1 {
+    private static final Logger LOGGER = LogManager.getRootLogger();
+
     public Map<String, Double> mapSummator(Map<String, ArrayList<? extends Number>> map) {
+        LOGGER.info("Creating new map");
         Map outMap = new HashMap<String, Double>();
         Set<Map.Entry<String, ArrayList<? extends Number>>> set = map.entrySet();
+        LOGGER.info("Calculating sum for ArrayList elements, and filling out map");
         for (Map.Entry<String, ArrayList<? extends Number>> entry : set) {
             Double sum = 0.0;
-            for (int i = 0; i < entry.getValue().size(); i++) {
-                sum += entry.getValue().get(i).doubleValue();
+            try {
+                for (int i = 0; i < entry.getValue().size(); i++) {
+                    sum += entry.getValue().get(i).doubleValue();
+                }
+                outMap.put(entry.getKey(), sum);
+            } catch (NullPointerException e) {
+                LOGGER.error("input map have null elements", e);
             }
-            outMap.put(entry.getKey(), sum);
         }
         return outMap;
     }
@@ -26,6 +37,8 @@ public class GenericTask1 {
     public static void main(String[] args) {
         Random random = new Random();
         Map inputMap = new HashMap<String, ArrayList<Integer>>();
+        LOGGER.entry();
+        LOGGER.info("filling input map");
         for (int i = 0; i < 10; i++) {
             ArrayList<Integer> list = new ArrayList<>();
             for (int j = 0; j < 10 + i; j++) {
@@ -35,18 +48,20 @@ public class GenericTask1 {
         }
         Set<Map.Entry<String, ArrayList<Integer>>> set = inputMap.entrySet();
         for (Map.Entry<String, ArrayList<Integer>> entry : set) {
-            System.out.println(entry.getKey() + "-" + entry.getValue().toString());
+            LOGGER.info(entry.getKey() + "-" + entry.getValue().toString());
         }
-        System.out.println();
+        LOGGER.info("input map filed");
 
         GenericTask1 test = new GenericTask1();
 
         Map outMap;
 
         outMap = test.mapSummator(inputMap);
+        LOGGER.info("out map filled");
         Set<Map.Entry<String, Double>> outSet = outMap.entrySet();
         for (Map.Entry<String, Double> entry : outSet) {
-            System.out.println(entry.getKey() + "-" + entry.getValue());
+            LOGGER.info(entry.getKey() + "-" + entry.getValue());
         }
+        LOGGER.exit();
     }
 }
--- 10_generics/com/nixsolutions/GenericTask2.java
+++ 10_generics/com/nixsolutions/GenericTask2.java
@@ -1,19 +1,24 @@
 package com.nixsolutions;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Created by Serko on 26.11.2015.
  */
 public class GenericTask2 {
+    private static final Logger LOGGER = LogManager.getRootLogger();
     public static void main(String[] args) {
+        LOGGER.entry();
         float input = 3.14f;
         FloatToDouble test = new FloatToDouble();
-        System.out.println(test.get(input).getClass().getCanonicalName());
+        LOGGER.trace(test.get(input).getClass().getCanonicalName());
 
         Integer[] array = new Integer[10];
         for (int i = 0; i < array.length; i++) {
             array[i] = i;
         }
         IntegerToString test2 = new IntegerToString();
-        System.out.println(test2.get(array).getClass().getCanonicalName() + "/" + test2.get(array));
+        LOGGER.exit(test2.get(array).getClass().getCanonicalName() + "/" + test2.get(array));
     }
 }

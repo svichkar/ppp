fd4bd96b75a7b72e8cfe9c8e86b8029cc0eb1c8b
jax_on <jax.o.fly@gmail.com>
2015-11-26 11:58:12 +0200

    lab 9

--- 09_threads/com/nixsolutions/Task1Runner.java
+++ 09_threads/com/nixsolutions/Task1Runner.java
@@ -10,10 +10,30 @@ public class Task1Runner {
 	public static void main(String[] args) {
 		BlockingQueue<Integer> sharedQ = new ArrayBlockingQueue<>(100);
 		ExecutorService runner = Executors.newFixedThreadPool(3);
+		int attempts = 0;
 		
-		runner.execute(new Producer(sharedQ));
-		runner.execute(new Consumer(sharedQ, "even"));
-		runner.execute(new Consumer(sharedQ, "odd"));
+		Consumer cons1 = new Consumer(sharedQ, "even");
+		Consumer cons2 = new Consumer(sharedQ, "odd");
+		Producer prod = new Producer(sharedQ);
+		
+		runner.execute(prod);
+		runner.execute(cons1);
+		runner.execute(cons2);
+		
+		while (sharedQ.isEmpty()) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println("waiting for an item");
+			attempts++;
+			if (attempts == 2) {
+				cons1.stop();
+				cons2.stop();
+				break;
+			}
+		}
 		runner.shutdown();
 	}
 }

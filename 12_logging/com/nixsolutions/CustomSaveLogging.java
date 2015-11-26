72b1ca64f3d1b5029d2852f5baa311cca8325512
kozlovskij <serkozlovskij@yandex.ru>
2015-11-26 15:01:28 +0200

    first version

--- 12_logging/com/nixsolutions/CustomSaveLogging.java
+++ 12_logging/com/nixsolutions/CustomSaveLogging.java
@@ -1,5 +1,8 @@
 package com.nixsolutions;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,24 +12,26 @@ import java.io.Writer;
  * Created by kozlovskij on 11/13/2015.
  */
 public class CustomSaveLogging implements exception.Save {
+    private static final Logger LOGGER = LogManager.getLogger(CustomSaveLogging.class);
+    private String text;
+    private String path;
     @Override
     public void save(String text, String path) throws RuntimeException {
-
+        this.text = text;
+        this.path = path;
         File file = new File(path);
-
-        if (!file.exists()) {
-            try {
-                file.createNewFile();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
         Writer writer = null;
         try {
+            if (!file.exists()) {
+                LOGGER.info("try creating file");
+                file.createNewFile();
+            }
+            LOGGER.info("try copying text");
             writer = new FileWriter(file, false);
             writer.write(text);
             writer.flush();
         } catch (IOException e) {
+            LOGGER.error("file or stream are not created");
             throw new RuntimeException(e);
         } finally {
             try {
@@ -34,8 +39,17 @@ public class CustomSaveLogging implements exception.Save {
                     writer.close();
                 }
             } catch (IOException e) {
+                LOGGER.warn("Stream isn't close");
                 throw new RuntimeException(e);
             }
         }
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getPath() {
+        return path;
     }
 }
--- 12_logging/com/nixsolutions/TestLogging.java
+++ 12_logging/com/nixsolutions/TestLogging.java
@@ -1,17 +1,27 @@
 package com.nixsolutions;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Created by kozlovskij on 11/13/2015.
  */
 public class TestLogging {
+    private static final Logger LOGGER = LogManager.getRootLogger();
     public static void main(String[] args) {
         CustomSaveLogging test = new CustomSaveLogging();
         try {
+            LOGGER.trace("start the copying process");
             test.save("test", "D:\\test.txt");
+            LOGGER.debug(test.getText() + "-" + test.getPath());
             test.save("test1", "");
+            LOGGER.debug(test.getText() + "-" + test.getPath());
             test.save("test2", "D:\\test.txt");
+            LOGGER.debug(test.getText() + "-" + test.getPath());
         } catch (RuntimeException e) {
-            System.out.printf("We caught exception: %s%n", e.toString());
+            LOGGER.error("We caught exception: ",  e);
+        } finally {
+            LOGGER.trace("application close");
         }
     }
 }

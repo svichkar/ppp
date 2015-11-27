95f920197b7ae425e2fd0f093779ea0596a721bd
Evgeniy <evgeniusf@gmail.com>
2015-11-27 16:15:24 +0200

    Serialization lab

--- 08_serialization/com/nixsolutions/serialization/Accounts.java
+++ 08_serialization/com/nixsolutions/serialization/Accounts.java
@@ -4,32 +4,37 @@ import java.io.Externalizable;
 import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
-import java.io.Serializable;
-
-import com.sun.org.apache.xml.internal.serializer.SerializationHandler;
 
 public class Accounts implements Externalizable {
 	transient Long id;
 	transient String name, role;
-	String strOut;
 
+	// Constructor with default values
+	public Accounts() {
+		this.id = 0l;
+		this.role = "";
+		this.name = "";
+
+	}
+
+	// Constructor with definite fields
 	public Accounts(Long id, String name, String role) {
 		this.id = id;
 		this.role = role;
 		this.name = name;
-		this.strOut = id + ";" + role + ";" + name + ";";
-	}
 
+	}
 	@Override
 	public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
-		// TODO Auto-generated method stub
-
+		String[] arrFromFile = ((String) in.readObject()).toString().split(";");
+		id = (long) Integer.parseInt(arrFromFile[0]);
+		role = arrFromFile[1];
+		name = arrFromFile[2];
 	}
 
 	@Override
 	public void writeExternal(ObjectOutput out) throws IOException {
-		// TODO Auto-generated method stub
-
+		out.writeObject(id + ";" + role + ";" + name + ";");
 	}
-
+	
 }
--- 08_serialization/com/nixsolutions/serialization/Main.java
+++ 08_serialization/com/nixsolutions/serialization/Main.java
@@ -1,24 +1,41 @@
 package com.nixsolutions.serialization;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		Accounts account = new Accounts(1l, "Vasya", "Student");
+		Accounts account = new Accounts(1l, "Vasya", "Student_Dvoe4nik");
+		Accounts deserializedAccount;
 		String path = System.getProperty("user.dir") + "\\account.dat";
-		try {
-			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path));
-			oos.writeObject(account);
 
+		System.out.println("Before serialization: " + account.toString());
+		System.out.println("ID = " + account.id.toString());
+		System.out.println("Role = " + account.role);
+		System.out.println("Name = " + account.name);
+
+		try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path))) {
+			oos.writeObject(account);
 			oos.close();
 		} catch (IOException ex) {
 			ex.printStackTrace();
-		} 
-		
-	}
+		}
+		try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path))) {
+			deserializedAccount = (Accounts) ois.readObject();
+			System.out.println("After serialization: " + deserializedAccount.toString());
+			System.out.println("ID = " + deserializedAccount.id.toString());
+			System.out.println("Role = " + deserializedAccount.role);
+			System.out.println("Name = " + deserializedAccount.name);
+			ois.close();
+		} catch (IOException | ClassNotFoundException ex) {
+			ex.printStackTrace();
+		}
 
+	}
 }

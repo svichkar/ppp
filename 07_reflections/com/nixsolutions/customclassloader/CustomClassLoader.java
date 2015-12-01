63ca39776a7923adcb1115a2dad3d8209f38ef97
rybkinrolla <vitaliy.rybkin@nixsolutions.com>
2015-12-01 12:52:33 +0200

    update

--- 07_reflections/com/nixsolutions/customclassloader/CustomClassLoader.java
+++ 07_reflections/com/nixsolutions/customclassloader/CustomClassLoader.java
@@ -2,15 +2,12 @@ package com.nixsolutions.customclassloader;
 
 import java.io.*;
 import org.apache.commons.io.FileUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 /**
  * Created by rybkinrolla on 28.11.2015.
  */
 public class CustomClassLoader extends ClassLoader implements PathClassLoader {
     private String pathNew;
-    private static final Logger logg = LogManager.getLogger("UtilTestConvertLogger");
 
     @Override
     public void setPath(String dir) {

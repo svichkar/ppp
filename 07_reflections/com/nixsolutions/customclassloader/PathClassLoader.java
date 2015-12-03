a8c2c2a06a743ec552b00911a38b959e8f2e6c95
rybkinrolla <vitaliy.rybkin@nixsolutions.com>
2015-12-03 10:24:42 +0200

    update

--- 07_reflections/com/nixsolutions/customclassloader/PathClassLoader.java
+++
@@ -1,9 +0,0 @@
-package com.nixsolutions.customclassloader;
-
-/**
- * Created by rybkinrolla on 28.11.2015.
- */
-public interface PathClassLoader {
-    void setPath(String dir);
-    String getPath();
-}

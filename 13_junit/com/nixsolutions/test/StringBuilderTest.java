17e61294c91e37f82933deae6c0bd29c59c4f21d
kozlovskij <s.kozlovskij@nixsolutions.com>
2015-12-08 14:46:39 +0200

    according to feedback

--- 13_junit/com/nixsolutions/test/StringBuilderTest.java
+++ 13_junit/com/nixsolutions/test/StringBuilderTest.java
@@ -1,141 +1,85 @@
 import org.junit.*;
 import org.junit.rules.ExpectedException;
 
+import java.util.Arrays;
+
 /**
  * Created by kozlovskij on 12/2/2015.
  */
 
 
 public class StringBuilderTest {
-
-    private static StringBuilder BUILDER;
-    private static String INPUTSTRING = "TestString!";
+    private StringBuilder builder;
+    private static String INPUTSTRING = "abc";
     private String testString = "someText";
-    private char[] chars = {'a', 'b', 'c', 'd', 'e', 'f'};
+    private char[] chars = {'d', 'e', 'f'};
 
     @Rule
     public ExpectedException exception = ExpectedException.none();
 
-    @BeforeClass
-    public static void createNewInstance() {
-        BUILDER = new StringBuilder();
-    }
-
     @Before
     public void setUp() {
-        BUILDER.append(INPUTSTRING);
-    }
-
-    @After
-    public void tearDown() {
-        BUILDER.delete(0, BUILDER.length());
+        builder =  new StringBuilder();
+        builder.append(INPUTSTRING);
     }
 
     @Test
     public void shouldAppendStringToTail() {
         String temp = INPUTSTRING + testString;
-        BUILDER.append(testString);
-        Assert.assertEquals(temp, BUILDER.toString());
+        builder.append(testString);
+        Assert.assertEquals(temp, builder.toString());
     }
 
     @Test
     public void shouldAppendCharSequenceToTail() {
-        String temp = INPUTSTRING;
-        for (int i = 0; i < chars.length; i++) {
-            temp += chars[i];
-        }
-        BUILDER.append(chars);
-        Assert.assertEquals(temp, BUILDER.toString());
+        String expectedString = "abcdef";
+        builder.append(chars);
+        Assert.assertEquals(expectedString,builder.toString());
     }
 
     @Test
     public void shouldInsertSpecifiedStringAtSpecifiedPosition() {
-        int position = (int) (Math.random() * BUILDER.length());
-        String temp = "";
-        for (int i = 0; i < INPUTSTRING.length(); i++) {
-            if (i != position) {
-                temp += INPUTSTRING.charAt(i);
-            } else {
-                for (int j = 0; j < testString.length(); j++) {
-                    temp += testString.charAt(j);
-                }
-                temp += INPUTSTRING.charAt(i);
-            }
-        }
-        BUILDER.insert(position, testString);
-        Assert.assertEquals(temp, BUILDER.toString());
+        String expectedString = "asomeTextbc";
+        builder.insert(1, testString);
+        Assert.assertEquals(expectedString, builder.toString());
     }
 
     @Test
-    public void shouldReturnExceptionWhenInsertInPositionGreaterThanLenth() throws IndexOutOfBoundsException {
-        int position = (int) (Math.random() * BUILDER.length()) + BUILDER.length() + 1;
+    public void shouldReturnExceptionWhenInsertInPositionGreaterThanLength() throws IndexOutOfBoundsException {
         exception.expect(IndexOutOfBoundsException.class);
-        BUILDER.insert(position, testString);
+        builder.insert(100, testString);
     }
 
     @Test
     public void shouldReplaceStringInRange() {
-        int position = (int) (Math.random() * BUILDER.length());
-        int endPosition = position + (int) (Math.random() * BUILDER.length());
-        String temp = "";
-        for (int i = 0; i < position; i++) {
-            temp += INPUTSTRING.charAt(i);
-        }
-        for (int i = 0; i < testString.length(); i++) {
-            temp += testString.charAt(i);
-        }
-        if (endPosition < INPUTSTRING.length()) {
-            for (int i = endPosition; i < INPUTSTRING.length(); i++) {
-                temp += INPUTSTRING.charAt(i);
-            }
-        }
-        BUILDER.replace(position, endPosition, testString);
-        Assert.assertEquals(temp, BUILDER.toString());
+        String expectedString = "someTextbc";
+        builder.replace(0, 1, testString);
+        Assert.assertEquals(expectedString, builder.toString());
     }
 
     @Test
     public void shouldDeleteSpecifiedRange() {
-        int position = (int) (Math.random() * BUILDER.length());
-        int endPosition = position + (int) (Math.random() * BUILDER.length());
-        String temp = "";
-        for (int i = 0; i < position; i++) {
-            temp += INPUTSTRING.charAt(i);
-        }
-        if (endPosition < INPUTSTRING.length()) {
-            for (int i = endPosition; i < INPUTSTRING.length(); i++) {
-                temp += INPUTSTRING.charAt(i);
-            }
-        }
-        BUILDER.delete(position, endPosition);
-        Assert.assertEquals(temp, BUILDER.toString());
+        String expectedString = "bc";
+        builder.delete(0, 1);
+        Assert.assertEquals(expectedString, builder.toString());
     }
 
     @Test
     public void shouldReverseString() {
-        String temp = "";
-        for (int i = INPUTSTRING.length() - 1; i >= 0; i--) {
-            temp += INPUTSTRING.charAt(i);
-        }
-        BUILDER.reverse();
-        Assert.assertEquals(temp, BUILDER.toString());
+        String expectedString = "cba";
+        builder.reverse();
+        Assert.assertEquals(expectedString, builder.toString());
     }
 
     @Test
     public void shouldReturnSubstringFromPositionToEnd() {
-        int position = (int) (Math.random() * BUILDER.length());
-        String temp = "";
-        for (int i = 0; i < INPUTSTRING.length(); i++) {
-            if (i >= position) {
-                temp += INPUTSTRING.charAt(i);
-            }
-        }
-        Assert.assertEquals(temp, BUILDER.substring(position));
+        String expectedString = "bc";
+        Assert.assertEquals(expectedString, builder.substring(1));
     }
 
     @Test
     public void shouldReturnExceptionWhenIndexForSubstringIsNegative() throws IndexOutOfBoundsException {
-        int position = (int) (Math.random() * BUILDER.length()) * -1;
         exception.expect(IndexOutOfBoundsException.class);
-        BUILDER.substring(position);
+        builder.substring(-1);
     }
 }

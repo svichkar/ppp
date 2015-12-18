7927ed3933971095e93d3c2e5a9dd17b5fda7eab
ivan_usanin <ivan.usanin@gmail.com>
2015-12-18 22:22:03 +0200

    Lab 19

--- Lab19_spring/src/main/java/com/nixsolutions/dao/impl/RoleDaoImpl.java
+++ Lab19_spring/src/main/java/com/nixsolutions/dao/impl/RoleDaoImpl.java
@@ -2,130 +2,86 @@ package com.nixsolutions.dao.impl;
 
 import com.nixsolutions.dao.RoleDao;
 import com.nixsolutions.entity.Role;
-import com.nixsolutions.util.HibernateUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.Criteria;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
+import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
+@Transactional
 public class RoleDaoImpl implements RoleDao {
 
-    private static Logger LOG = LogManager.getLogger(RoleDaoImpl.class.getName());
+	private static Logger LOG = LogManager.getLogger(RoleDaoImpl.class.getName());
+	@Autowired
+	private SessionFactory sessionFactory;
 
-    public RoleDaoImpl() {
-    }
+	public RoleDaoImpl() {
+	}
 
-    public void create(Role role) {
-        Session session = null;
-        Transaction transaction = null;
-        try {
-            session = HibernateUtil.getSessionFactory().getCurrentSession();
-            transaction = session.beginTransaction();
-            session.save(role);
-            transaction.commit();
-        } catch (Exception ex) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            LOG.error(ex);
-        }
-    }
+	public void create(Role role) {
+		try {
+			sessionFactory.getCurrentSession().save(role);
+		} catch (Exception ex) {
+			LOG.error(ex);
+		}
+	}
 
-    public void update(Role role) {
-        Session session = null;
-        Transaction transaction = null;
-        try {
-            session = HibernateUtil.getSessionFactory().getCurrentSession();
-            transaction = session.beginTransaction();
-            session.saveOrUpdate(role);
-            transaction.commit();
-        } catch (Exception ex) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            LOG.error(ex);
-        }
-    }
+	public void update(Role role) {
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(role);
+		} catch (Exception ex) {
+			LOG.error(ex);
+		}
+	}
 
-    public void delete(Role role) {
-        Session session = null;
-        Transaction transaction = null;
-        try {
-            session = HibernateUtil.getSessionFactory().getCurrentSession();
-            transaction = session.beginTransaction();
-            session.delete(role);
-            transaction.commit();
-        } catch (Exception ex) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            LOG.error(ex);
-        }
-    }
+	public void delete(Role role) {
 
-    public Role getByRoleId(int roleId) {
-        Role role = new Role();
-        Session session = null;
-        Transaction transaction = null;
-        try {
-            session = HibernateUtil.getSessionFactory().getCurrentSession();
-            transaction = session.beginTransaction();
-            role = (Role)session.get(Role.class, roleId);
-            transaction.commit();
-        } catch (Exception ex) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            LOG.error(ex);
-        }
-        return role;
-    }
+		try {
+			sessionFactory.getCurrentSession().delete(role);
+		} catch (Exception ex) {
+			LOG.error(ex);
+		}
+	}
 
-    @SuppressWarnings("unchecked")
+	public Role getByRoleId(int roleId) {
+		Role role = new Role();
+		try {
+			role = (Role) sessionFactory.getCurrentSession().get(Role.class, roleId);
+		} catch (Exception ex) {
+			LOG.error(ex);
+		}
+		return role;
+	}
+
+	@SuppressWarnings("unchecked")
 	public Role getByRoleName(String roleName) {
-        Role role = new Role();
-        Session session = null;
-        Transaction transaction = null;
-        try {
-            session = HibernateUtil.getSessionFactory().getCurrentSession();
-            transaction = session.beginTransaction();
-            Criteria c = session.createCriteria(Role.class);
-            c.add(Restrictions.eq("roleName", roleName));
-            List<Role> list = c.list();
-            role = (Role) list.get(0);
-            transaction.commit();
-        } catch (Exception ex) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            LOG.error(ex);
-        }
-        return role;
-    }
+		Role role = new Role();
+		try {
+			Criteria c = sessionFactory.getCurrentSession().createCriteria(Role.class);
+			c.add(Restrictions.eq("roleName", roleName));
+			List<Role> list = c.list();
+			role = (Role) list.get(0);
+		} catch (Exception ex) {
+			LOG.error(ex);
+		}
+		return role;
+	}
 
-    @SuppressWarnings("unchecked")
+	@SuppressWarnings("unchecked")
 	public List<Role> getAll() {
-        List<Role> toReturn = new ArrayList<Role>();
-        Session session = null;
-        Transaction transaction = null;
-        try {
-            session = HibernateUtil.getSessionFactory().getCurrentSession();
-            transaction = session.beginTransaction();
-            toReturn.addAll(session.createCriteria(Role.class).list());
-            transaction.commit();
-        } catch (Exception ex) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            LOG.error(ex);
-        }
-        return toReturn;
-    }
+		List<Role> toReturn = new ArrayList<Role>();
+		try {
+			toReturn.addAll(sessionFactory.getCurrentSession().createCriteria(Role.class).list());
+		} catch (Exception ex) {
+			LOG.error(ex);
+		}
+		return toReturn;
+	}
 }

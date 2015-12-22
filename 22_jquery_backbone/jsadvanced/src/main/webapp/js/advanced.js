60a9981e5fadeccdf8a31041887389ca94bfc18f
unknown <kulishov@mkulishov.nixsolutions.com>
2015-12-22 18:45:28 +0200

    backbone changes

--- 22_jquery_backbone/jsadvanced/src/main/webapp/js/advanced.js
+++ 22_jquery_backbone/jsadvanced/src/main/webapp/js/advanced.js
@@ -1,35 +1,97 @@
-var AppRouter = Backbone.Router.extend({
-	routes: {
-		//
-	}
-});
+document.onreadystatechange = function() {
+	var AppRouter = Backbone.Router.extend({
+		viewAll : null,
+		viewOne : null,
+		routes : {
+			"/admin/addCar.do" : "add",
+			"/admin/editCar.do" : "edit",
+			"/allCars" : "all"
+		// "/deleteCar.do": "delete"
+		},
+		initialize : function() {
+			this.viewAll = new AllCarsView();
+			this.viewOne = new CarView();
+		}
+	});
 
-var app_router = new AppRouter;
-app_router.on('route:getPost', function(id) {
-	
-});
-Backbone.history.start();
+	var app_router = new AppRouter;
+	/*
+	 * app_router.on('route:getPost', function(id) {
+	 * 
+	 * });
+	 */
+	Backbone.history.start();
 
+	var CarModel = Backbone.Model.extend({
+		initialize : function() {
+			console.log('Car created.');
+		},
+		url_root : '/services/car',
+		defaults : {
+			id : '',
+			description : '',
+			model : '',
+			vin : '',
+			customer_id : null
+		},
+		/*
+		 * fetchCurrent : function(id, options) { options = options || {}; if
+		 * (options.url === undefined) { options.url = this.urlRoot +
+		 * "/getById?carId=" + id; } return
+		 * Backbone.Model.prototype.fetch.call(this, options); }
+		 */
+		getCustomUrl : function(method) {
+			switch (method) {
+			case 'read':
+				return this.url_root + '/getById?carId=' + this.id;
+				break;
+			case 'create':
+				return this.url_root + '/add';
+				break;
+			case 'update':
+				return this.url_root + '/update';
+				break;
+			case 'delete':
+				return this.url_root + '/delete';
+				break;
+			}
+		},
+		sync : function(method, model, options) {
+			options || (options = {});
+			options.url = this.getCustomUrl(method.toLowerCase());
+			return Backbone.sync.apply(this, arguments);
+		}
+	});
 
+	var CarCollection = Backbone.Collection.extend({
+		model : CarModel,
+		url : '/services/car/getAll'
+	});
 
-var CarModel = Backbone.Model.extend({
-	initialize: function() {
-		console.log('Car created.');
-	},
-	url_root: '/services/car',
-	defaults: {
-		car_id: '',
-		description: '',
-		model: '',
-		vin: '',
-		customer_id: null
-	},
-	fetchCurrent: function (id, options) {
-        options = options || {};
-        if (options.url === undefined) {
-            options.url = this.urlRoot + "?carId=" + id;
-        }
-        return Backbone.Model.prototype.fetch.call(this, options);
-    }
-});
-
+	var AllCarsView = Backbone.View.extend({
+		template : 'carsPage',
+		render : function() {
+			var that = this;
+			$.get("/WEB-INF/jsp/" + this.template + ".jsp", function(template) {
+				var html = $(template).tmpl();
+				that.$el.html(html);
+			});
+			return this;
+		}
+	});
+	//
+	var CarView = Backbone.View.extend({
+		template : 'car',
+		initialize : function(options) {
+			this.options = options || {};
+		},
+		render : function() {
+			var that = this;
+			$.get("/WEB-INF/jsp/" + this.template + ".jsp", function(template) {
+				var html = $(template).tmpl();
+				that.$el.html(html);
+			});
+			return this;
+		}
+	});
+}

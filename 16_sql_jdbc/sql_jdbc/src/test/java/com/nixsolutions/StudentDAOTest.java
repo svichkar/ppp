e354f2eb4e6b4e5d7bb6f7fea557457e08701258
elena_z <lena_zr@nixsolutions.com>
2016-01-02 17:50:03 +0200

    Laba 16

---
+++ 16_sql_jdbc/sql_jdbc/src/test/java/com/nixsolutions/StudentDAOTest.java
@@ -0,0 +1,106 @@
+package com.nixsolutions;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.dbunit.DatabaseUnitException;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.database.QueryDataSet;
+import org.dbunit.dataset.DataSetException;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ITable;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.nixsolutions.dao.DAOFactory;
+import com.nixsolutions.dao.impl.StudentDAOImpl;
+import com.nixsolutions.entity.Student;
+import com.nixsolutions.util.ConnectionManager;
+
+public class StudentDAOTest {
+	private Connection conn;
+	private IDatabaseConnection iconn;
+	private StudentDAOImpl student;
+
+	@Before
+	public void setUp() throws DatabaseUnitException {
+		conn = ConnectionManager.getConnection();
+		iconn = new DatabaseConnection(conn);
+		student = DAOFactory.getStudent();
+	}
+
+	@After
+	public void tearDown() throws SQLException {
+		conn.close();
+		iconn.close();
+	}
+
+	@Test
+	public void shouldCreateStudent() throws DataSetException {
+		Student studentNew = student.createStudent(8, "Mark", "Grey", 1, Date.valueOf("2014-09-01"), 1, 2);
+		QueryDataSet qDataSet = new QueryDataSet(iconn);
+		qDataSet.addTable("student", "SELECT * FROM student");
+		IDataSet ds = qDataSet;
+		ITable table = ds.getTable("student");
+		Assert.assertEquals(studentNew.getFirstName(), table.getValue(studentNew.getStudentId() - 1, "first_name"));
+		Assert.assertEquals(studentNew.getLastName(), table.getValue(studentNew.getStudentId() - 1, "last_name"));
+		student.deleteStudent(studentNew);
+	}
+
+	@Test
+	public void shouldUpdateStudent() throws DataSetException {
+		student.createStudent(8, "Mark", "Grey", 1, Date.valueOf("2014-09-01"), 1, 2);
+		Student studentUpdate = new Student(8, "Mark", "Test", 1, Date.valueOf("2014-09-01"), 1, 2);
+		student.updateStudent(studentUpdate);
+		QueryDataSet qDataSet = new QueryDataSet(iconn);
+		qDataSet.addTable("student", "SELECT * FROM student");
+		IDataSet ds = qDataSet;
+		ITable table = ds.getTable("student");
+		Assert.assertEquals(studentUpdate.getFirstName(), table.getValue(studentUpdate.getStudentId() - 1, "first_name"));
+		Assert.assertEquals(studentUpdate.getLastName(), table.getValue(studentUpdate.getStudentId() - 1, "last_name"));
+		student.deleteStudent(studentUpdate);
+	}
+
+	@Test
+	public void shouldDeleteStudent() throws DataSetException {
+		Student studentDelete = student.createStudent(8, "Mark", "Grey", 1, Date.valueOf("2014-09-01"), 1, 2);
+		QueryDataSet qDataSetBefore = new QueryDataSet(iconn);
+		qDataSetBefore.addTable("student", "SELECT * FROM student");
+		IDataSet dataSetBefore = qDataSetBefore;
+		ITable tableBefore = dataSetBefore.getTable("student");
+		int sizeBefore = tableBefore.getRowCount();
+		student.deleteStudent(studentDelete);
+		QueryDataSet qDataSetAfter = new QueryDataSet(iconn);
+		qDataSetAfter.addTable("student", "SELECT * FROM student");
+		IDataSet dataSetAfter = qDataSetAfter;
+		ITable tableAfter = dataSetAfter.getTable("student");
+		int sizeAfter = tableAfter.getRowCount();
+		Assert.assertEquals(sizeBefore - 1, sizeAfter);
+	}
+
+	@Test
+	public void shouldGetAllStudents() throws DataSetException {
+		List<Student> students = student.findAllStudents();
+		QueryDataSet qDataSet = new QueryDataSet(iconn);
+		qDataSet.addTable("student", "SELECT * FROM student");
+		IDataSet ds = qDataSet;
+		ITable table = ds.getTable("student");
+		Assert.assertEquals(table.getRowCount(), students.size());
+	}
+
+	@Test
+	public void shouldFindStudentById() throws DataSetException {
+		Student studentTest = student.findStudentById(5);
+		QueryDataSet qDataSet = new QueryDataSet(iconn);
+		qDataSet.addTable("student", "SELECT * FROM student");
+		IDataSet ds = qDataSet;
+		ITable table = ds.getTable("student");
+		Assert.assertEquals(table.getValue(studentTest.getStudentId() - 1, "first_name"), studentTest.getFirstName());
+		Assert.assertEquals(table.getValue(studentTest.getStudentId() - 1, "last_name"), studentTest.getLastName());		
+	}
+}
---
+++ 16_sql_jdbc/sql_jdbc/src/test/java/com/nixsolutions/TermDAOTest.java
@@ -0,0 +1,102 @@
+package com.nixsolutions;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.dbunit.DatabaseUnitException;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.database.QueryDataSet;
+import org.dbunit.dataset.DataSetException;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ITable;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.nixsolutions.dao.DAOFactory;
+import com.nixsolutions.dao.impl.TermDAOImpl;
+import com.nixsolutions.entity.Term;
+import com.nixsolutions.util.ConnectionManager;
+
+public class TermDAOTest {
+	private Connection conn;
+	private IDatabaseConnection iconn;
+	private TermDAOImpl term;
+
+	@Before
+	public void setUp() throws DatabaseUnitException {
+		conn = ConnectionManager.getConnection();
+		iconn = new DatabaseConnection(conn);
+		term = DAOFactory.getTerm();
+	}
+
+	@After
+	public void tearDown() throws SQLException {
+		conn.close();
+		iconn.close();
+	}
+
+	@Test
+	public void shouldCreateTerm() throws DataSetException {
+		Term termNew = term.createTerm(5, "Autumn-2016");
+		QueryDataSet qDataSet = new QueryDataSet(iconn);
+		qDataSet.addTable("term", "SELECT * FROM term");
+		IDataSet ds = qDataSet;
+		ITable table = ds.getTable("term");
+		Assert.assertEquals(termNew.getTermName(), table.getValue(termNew.getTermId() - 1, "term_name"));
+		term.deleteTerm(termNew);
+	}
+
+	@Test
+	public void shouldUpdateTerm() throws DataSetException {
+		term.createTerm(5, "Autumn-2016");
+		Term termUpdate = new Term(5, "Autumn-2017");
+		term.updateTerm(termUpdate);
+		QueryDataSet qDataSet = new QueryDataSet(iconn);
+		qDataSet.addTable("term", "SELECT * FROM term");
+		IDataSet ds = qDataSet;
+		ITable table = ds.getTable("term");
+		Assert.assertEquals(termUpdate.getTermName(), table.getValue(termUpdate.getTermId() - 1, "term_name"));
+		term.deleteTerm(termUpdate);
+	}
+
+	@Test
+	public void shouldDeleteTerm() throws DataSetException {
+		Term termDelete = term.createTerm(5, "Autumn-2016");
+		QueryDataSet qDataSetBefore = new QueryDataSet(iconn);
+		qDataSetBefore.addTable("term", "SELECT * FROM term");
+		IDataSet dataSetBefore = qDataSetBefore;
+		ITable tableBefore = dataSetBefore.getTable("term");
+		int sizeBefore = tableBefore.getRowCount();
+		term.deleteTerm(termDelete);
+		QueryDataSet qDataSetAfter = new QueryDataSet(iconn);
+		qDataSetAfter.addTable("term", "SELECT * FROM term");
+		IDataSet dataSetAfter = qDataSetAfter;
+		ITable tableAfter = dataSetAfter.getTable("term");
+		int sizeAfter = tableAfter.getRowCount();
+		Assert.assertEquals(sizeBefore - 1, sizeAfter);
+	}
+
+	@Test
+	public void shouldGetAllTerms() throws DataSetException {
+		List<Term> terms = term.findAllTerms();
+		QueryDataSet qDataSet = new QueryDataSet(iconn);
+		qDataSet.addTable("term", "SELECT * FROM term");
+		IDataSet ds = qDataSet;
+		ITable table = ds.getTable("Term");
+		Assert.assertEquals(table.getRowCount(), terms.size());
+	}
+
+	@Test
+	public void shouldFindTermById() throws DataSetException {
+		Term termTest = term.findTermById(3);
+		QueryDataSet qDataSet = new QueryDataSet(iconn);
+		qDataSet.addTable("term", "SELECT * FROM term");
+		IDataSet ds = qDataSet;
+		ITable table = ds.getTable("term");
+		Assert.assertEquals(table.getValue(termTest.getTermId() - 1, "term_name"), termTest.getTermName());
+	}
+}

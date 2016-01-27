fa72b8f61a5f4b10fee568c68eeb2a0d8d5b419c
Zinovyi Dmitro <liberal4ik@gmail.com>
2016-01-27 13:41:05 +0200

    add full answer 04_dates

--- 04_dates/com/nixsolutions/DateCollections.java
+++ 04_dates/com/nixsolutions/DateCollections.java
@@ -1,9 +1,8 @@
 package com.nixsolutions;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.time.*;
-
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.Calendar;
@@ -12,9 +11,10 @@ import java.util.Locale;
 
 public class DateCollections {
 
+    public static String[] months = {"JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER",
+            "OCTOBER", "NOVEMBER", "DECEMBER"};
+
     public static void daysInEachMonth(int year) {
-        String[] months = {"JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER",
-                "OCTOBER", "NOVEMBER", "DECEMBER"};
         Calendar c = Calendar.getInstance();
         c.set(Calendar.YEAR, year);
         for (int i = 0; i < 12; i++) {
@@ -24,6 +24,10 @@ public class DateCollections {
     }
 
     public static void mondaysInMonth(int year, int month) {
+        if (month < 0 || month > 11) {
+            System.out.println("Incorrect month. Please set 0-11 format.");
+            return;
+        }
         Calendar c = Calendar.getInstance();
         c.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
         c.set(Calendar.DAY_OF_WEEK_IN_MONTH, 1);
@@ -48,13 +52,25 @@ public class DateCollections {
         return false;
     }
 
-    public static String timeGone(Date date) {
+    public static String timeGoneJava7(Date date) {
         long secDiff = new Date().getTime() - date.getTime();
         if (secDiff >= 0) {
             Calendar c = Calendar.getInstance();
             c.setTimeInMillis(secDiff);
             return (c.get(Calendar.YEAR) - 1970) + " year, " + (c.get(Calendar.MONTH)) + " month, "
-                    + (c.get(Calendar.DAY_OF_MONTH) - 1) + " day";
+                    + (c.get(Calendar.DAY_OF_MONTH )) + " day";
+        } else {
+            return "Input date bigger then current date";
+        }
+    }
+
+    public static String timeGoneJava8(Date date) {
+        long secDiff = new Date().getTime() - date.getTime();
+        if (secDiff >= 0) {
+            LocalDate today = LocalDate.now();
+            LocalDate yesterday = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            Period oneDay = Period.between(today, yesterday);
+            return oneDay.toString();
         } else {
             return "Input date bigger then current date";
         }
@@ -63,7 +79,11 @@ public class DateCollections {
     public static void printCurrentDateForSomeLocal() {
         LocalDate localDate = LocalDate.now();
         DateTimeFormatter dtf = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL);
-        System.out.println(localDate.format(dtf.withLocale(Locale.GERMAN)));
-        System.out.println(localDate.format(dtf.withLocale(Locale.CANADA)));
+        System.out.println("Canada: " + localDate.format(dtf.withLocale(Locale.CANADA)));
+        System.out.println("Germany: " + localDate.format(dtf.withLocale(Locale.GERMANY)));
+        System.out.println("Pakistan(used English language): " + localDate.format(dtf.withLocale(
+                new Locale("en", "PK"))));
+        System.out.println("Vietnam: " + localDate.format(dtf.withLocale(new Locale("vi", "VN"))));
     }
+
 }
--- 04_dates/com/nixsolutions/TestDateCollections.java
+++ 04_dates/com/nixsolutions/TestDateCollections.java
@@ -7,27 +7,33 @@ import java.util.Date;
 public class TestDateCollections {
 
     public static void main(String[] argc) {
+        //test data
+        int year = 2016;
+        int month = 11; //0-11 need set (as in the java.util.Calendar format)
+        String pattern = "dd/MM/yyyy";
+        String dateString = "01/12/1980";
 
-        //DateCollections.daysInEachMonth(2015);
-
-       //DateCollections.mondaysInMonth(2016, 9);
-
-
-        /*String d = "13/01/2017";
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        System.out.println("1. Number of days in each month for " + year + " year:");
+        DateCollections.daysInEachMonth(year);
+        System.out.println();
+        System.out.println("2. Days Monday for " + year + " year and " + month + " month: ");
+        DateCollections.mondaysInMonth(year, month);
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        sdf.setLenient(false);
+        System.out.println();
         try {
-            Date date = sdf.parse(d);
-            System.out.println(DateCollections.isFriday13(date));
-            System.out.println(DateCollections.timeGone(date));
+            Date date = sdf.parse(dateString);
+            System.out.println("3. Is " + dateString + " friday 13? " + DateCollections.isFriday13(date));
+            System.out.println();
+            System.out.println("4. How many time gone after " + dateString + "?");
+            System.out.println("Java 7: " + DateCollections.timeGoneJava7(date));
+            System.out.println("Java 8: " + DateCollections.timeGoneJava8(date));
         } catch (ParseException e) {
-            e.printStackTrace();
-        }*/
-
+            System.out.println("Incorrect format date.");
+        }
+        System.out.println();
+        System.out.println("5. Current date using different locale:");
         DateCollections.printCurrentDateForSomeLocal();
-
-
-
-
-
     }
+
 }

4cc5cbf5cef2ac27f84ac3dcd85d3ac385107e42
grhrd <dmitriy.beskrovniy@nixsolutions.com>
2016-01-28 14:56:27 +0200

    code formatting

--- 01_base_types/com/nixsolutions/Arrays.java
+++ 01_base_types/com/nixsolutions/Arrays.java
@@ -1,65 +1,73 @@
 // Arrays.java
 package com.nixsolutions;
+
 import java.util.Random;
 
 public class Arrays {
-	static final int LENGTH = 10000;
-	static final int LOOPS = 20;
-	private static void sort(int[] a){
-		boolean swapped = true;
-		while (swapped) {
-			swapped = false;
-			for(int i = 1; i < a.length; i++) {
-				if (a[i-1] > a[i]) {
-					int temp = a[i-1];
-					a[i-1] = a[i];
-					a[i] = temp;
-					swapped = true;
-				}
-			}
-		}
-	}
-	private int[] randFillArray(int arrayLength) {
-		Random rand = new Random();
-		int[] a = new int[arrayLength];
-		for(int i = 0; i < a.length; i++) {
-			a[i] = rand.nextInt(200)-100;
-		}
-		return a;
-	}
-	private long avg(long[] a) {
-		long sum = 0;
-		for(int i = 0; i < a.length; i++)
-			sum = sum + a[i];
-		return sum;
-	}
-	private void print(long x, long y){
-		System.out.println("Average execution times for each iteration (in seconds):\n");
-		System.out.println("com.nixsolutions.Arrays.sort: " + x/1e9);
-		System.out.println("java.util.Arrays.sort: " + y/1e9);
-	}
-	public static void main(String[] args) {
-		Arrays arrays = new Arrays();
-		long[][] results = new long[2][LENGTH];
-		System.out.println("Sorting arrays " + LOOPS + " times by each of the methods...\n");
-		for(int i = 0; i < LOOPS; i++) {
-			int[] orig = arrays.randFillArray(LENGTH);
-			int[] copy = new int[LENGTH];
-			long start, stop;
+  static final int LENGTH = 10000;
+  static final int LOOPS = 20;
+  
+  private static void sort(int[] a){
+    boolean swapped = true;
+    while (swapped) {
+      swapped = false;
+      for(int i = 1; i < a.length; i++) {
+        if (a[i-1] > a[i]) {
+          int temp = a[i-1];
+          a[i-1] = a[i];
+          a[i] = temp;
+          swapped = true;
+        }
+      }
+    }
+  }
+  
+  private int[] randFillArray(int arrayLength) {
+    Random rand = new Random();
+    int[] a = new int[arrayLength];
+    for (int i = 0; i < a.length; i++) {
+      a[i] = rand.nextInt(200)-100;
+    }
+    return a;
+  }
+  
+  private long avg(long[] a) {
+    long sum = 0;
+    for (int i = 0; i < a.length; i++) {
+      sum = sum + a[i];
+    }
+    return sum;
+  }
+  
+  private void print(long x, long y){
+    System.out.println("Average execution times for each iteration (in seconds):\n");
+    System.out.println("com.nixsolutions.Arrays.sort: " + x/1e9);
+    System.out.println("java.util.Arrays.sort: " + y/1e9);
+  }
+  
+  public static void main(String[] args) {
+    Arrays arrays = new Arrays();
+    long[][] results = new long[2][LENGTH];
+    System.out.println("Sorting arrays " + LOOPS + " times by each of the methods...\n");
+    for (int i = 0; i < LOOPS; i++) {
+      int[] orig = arrays.randFillArray(LENGTH);
+      int[] copy = new int[LENGTH];
+      long start, stop;
 
-			System.arraycopy(orig, 0, copy, 0, LENGTH);
-			start = System.nanoTime();
-			arrays.sort(copy);
-			stop = System.nanoTime();
-			results[0][i] = stop - start;
+      System.arraycopy(orig, 0, copy, 0, LENGTH);
+      start = System.nanoTime();
+      arrays.sort(copy);
+      stop = System.nanoTime();
+      results[0][i] = stop - start;
 
-			System.arraycopy(orig, 0, copy, 0, LENGTH);
-			start = System.nanoTime();
-			java.util.Arrays.sort(copy);
-			stop = System.nanoTime();
-			results[1][i] = stop - start;
-		}
-		arrays.print(arrays.avg(results[0]),
-		             arrays.avg(results[1]));
-	}
+      System.arraycopy(orig, 0, copy, 0, LENGTH);
+      start = System.nanoTime();
+      java.util.Arrays.sort(copy);
+      stop = System.nanoTime();
+      results[1][i] = stop - start;
+    }
+    arrays.print(arrays.avg(results[0]),
+                 arrays.avg(results[1]));
+  }
+  
 }
--- 01_base_types/com/nixsolutions/Float.java
+++ 01_base_types/com/nixsolutions/Float.java
@@ -1,26 +1,29 @@
 // Float.java
 package com.nixsolutions;
+
 import java.util.Scanner;
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
 
 public class Float {
-	public static void main(String[] args) {
-		String e = "e";
-		while (true) {
-			System.out.print("Please enter floating point number: ");
-			Scanner scan = new Scanner(System.in);
-			String s = scan.next();
-			BigDecimal bd = new BigDecimal(s);
-			if (s.toLowerCase().contains(e)) {
-				System.out.println("Received a number in exponential format");
-				System.out.println("This number in normal format is: " + bd.toPlainString());
-			} else {
-				System.out.println("Received a number in normal format");
-				DecimalFormat fmt = new DecimalFormat("0.00E00");
-				String fs = fmt.format(bd.doubleValue());
-				System.out.println("This number in exponential format is: " + fmt.format(bd.doubleValue()));
-			}
-		}
-	}
+  public static void main(String[] args) {
+    String e = "e";
+    while (true) {
+      System.out.print("Please enter floating point number: ");
+      Scanner scan = new Scanner(System.in);
+      String s = scan.next();
+      BigDecimal bd = new BigDecimal(s);
+      
+      if (s.toLowerCase().contains(e)) {
+        System.out.println("Received a number in exponential format");
+        System.out.println("This number in normal format: " + bd.toPlainString());
+      } else {
+        System.out.println("Received a number in normal format");
+        DecimalFormat fmt = new DecimalFormat("0.00E00");
+        String fs = fmt.format(bd.doubleValue());
+        System.out.println("This number in exponential format: " + fmt.format(bd.doubleValue()));
+      }
+    }
+  }
+  
 }

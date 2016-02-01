fea6f80ffbeab0a135c43c4abf7d2dcc6a648592
kozlovskij <serkozlovskij@yandex.ru>
2016-02-01 23:42:28 +0200

    new project

---
+++ 19_spring/Spring/src/main/java/com/nixsolutions/spring/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.nixsolutions.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Serko on 01.02.2016.
+ */
+@Controller
+public class MainController {
+    @RequestMapping("/main")
+    public String main (@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "mainPage.jsp";
+    }
+}
---
+++ 19_spring/Spring/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<a href="main?name=Sergey">Main Page</a>
+</body>
+</html>

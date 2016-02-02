8a5ca1978fe765f2ee0d9942651e6d195c7c23a5
kozlovskij <s.kozlovskij@nixsolutions.com>
2016-02-02 14:21:49 +0200

    first controller

--- 19_spring/Spring/src/main/java/com/nixsolutions/spring/controller/MainController.java
+++ 19_spring/Spring/src/main/java/com/nixsolutions/spring/controller/MainController.java
@@ -1,18 +1,25 @@
 package com.nixsolutions.spring.controller;
 
+import com.nixsolutions.spring.dao.TicketDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by Serko on 01.02.2016.
  */
+
 @Controller
 public class MainController {
-    @RequestMapping("/main")
-    public String main (@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "mainPage";
+    @Autowired
+    private TicketDAO ticketDAO;
+
+    @RequestMapping(value = "/main", method = RequestMethod.GET)
+    public String main(Model model) {
+        model.addAttribute("overdueTickets", ticketDAO.findOverdueTicket());
+        return "homePage";
     }
 }
--- 19_spring/Spring/target/Spring/index.jsp
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-<h2>Hello World!</h2>
-<a href="main?name=Sergey">Main Page</a>
-</body>
-</html>

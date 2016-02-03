37dd67f09420ac6f15e66951a30991b24ad55f74
Alexey Sobolenko <a.sobolenko@nixsolutions.com>
2016-02-03 12:43:42 +0200

    exception  enter the commit message for your changes. Lines starting

--- 05_exceptions/src/com/nixsolutions/CustomException.java
+++ 05_exceptions/src/com/nixsolutions/CustomException.java
@@ -2,14 +2,14 @@ package com.nixsolutions;
 /**
  * Created by sobolenko on 2/2/2016.
  */
-public class CustomExeption extends RuntimeException {
+public class CustomException extends RuntimeException {
     String message = "";
 
-    public CustomExeption(Throwable cause) {
+    public CustomException(Throwable cause) {
         super(cause);
     }
 
-    public CustomExeption(String message) {
+    public CustomException(String message) {
         this.message = message;
     }
 
--- 05_exceptions/src/com/nixsolutions/SaveFile.java
+++ 05_exceptions/src/com/nixsolutions/SaveFile.java
@@ -1,4 +1,5 @@
 package com.nixsolutions;
+
 import exception.Save;
 
 import javax.swing.*;
@@ -10,39 +11,47 @@ import java.io.IOException;
  * Created by sobolenko on 2/2/2016.
  */
 public class SaveFile implements Save {
+    /**
+     * Rewrite file, if this option has been chosen
+     *
+     * @param data       which write to file
+     * @param outputFile file name
+     */
+    public static void reWrite(String data, File outputFile) {
+        try {
+            outputFile.delete();
+            FileWriter fwriter = new FileWriter(outputFile);
+            fwriter.write(data);
+            fwriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
-    public void save(String data, String fileName){
-        String pathName = System.getProperty("user.dir");
+    public void save(String data, String fileName) {
         File outputFile = new File(fileName);
-        boolean reWrite = false;
         FileWriter fwriter = null;
         try {
             if (!outputFile.exists()) {
                 fwriter = new FileWriter(outputFile);
                 fwriter.write("");
             } else {
-                throw new CustomExeption("File already exists");
+                throw new CustomException("File already exists");
             }
             fwriter.write(data);
             fwriter.close();
         } catch (IOException io) {
-            throw new CustomExeption(io);
+            throw new CustomException(io);
+        } catch (CustomException ex) {
+            JFrame frame = new JFrame();
+            int result = JOptionPane.showConfirmDialog(frame, "File already exists, overwrite it?", "", JOptionPane.OK_CANCEL_OPTION);
+            frame.dispose();
+            if (result == 0) {
+                reWrite(data, outputFile);
+            } else {
+                ex.printStackTrace();
+            }
         }
     }
 }
-
-/*JFrame frame = new JFrame();
-int result = JOptionPane.showConfirmDialog(frame, "File already exists, overwrite it?", "", JOptionPane.OK_CANCEL_OPTION);
-frame.dispose();
-        if (result == 0) {
-        try {
-        outputFile.delete();
-        fwriter = new FileWriter(outputFile);
-        fwriter.write(data);
-        fwriter.close();
-        } catch (IOException e) {
-        e.printStackTrace();
-        }
-        }
-        }
-        }*/

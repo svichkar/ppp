5c17768277987d891c3ba3986d40f04e9a655516
Valeriy Pantiukhin <107pavalvl@gmail.com>
2016-02-08 17:26:21 +0200

    Exceptions: second attempt

--- 05_exceptions/src/com/nixsolutions/Save.java
+++ 05_exceptions/src/com/nixsolutions/Save.java
@@ -1,9 +1,11 @@
 package com.nixsolutions;
 
+import java.io.IOException;
+
 /**
  * Created by PAVALVL on 2/3/2016.
  */
 public interface Save {
-    void save(String stringToBeSaved, String absolutePath);
+    void save(String stringToBeSaved, String absolutePath) throws Exception;
 }
 
--- 05_exceptions/src/com/nixsolutions/SaveFile.java
+++ 05_exceptions/src/com/nixsolutions/SaveFile.java
@@ -10,68 +10,51 @@ import java.io.IOException;
  * Created by PAVALVL on 2/3/2016.
  */
 public class SaveFile implements Save {
-    private File fileName;
+    private File fileName = new File("fileToBeSaved.txt");
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         SaveFile saveFile = new SaveFile();
-        saveFile.prepareFileForSaving();
+        try {
+            saveFile.prepareFileForSaving();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     /**
      * Implements the save method from the Save interface
      */
-    public void save(String stringToBeSaved, String absolutePath) {
-        try {
-            if (!fileName.exists()) {
-                if (fileName.createNewFile()) {
-                    writeToFile(stringToBeSaved, absolutePath);
-                } else {
-                    throw new Exception("The file with the name " + fileName + " has not been created");
-                }
-            } else {
-                throw new IOException("The file with the name " + fileName + " already exists");
-            }
-        } catch (IOException e) {
-            if (fileName.delete()) {
-                save(stringToBeSaved, absolutePath);
-            } else {
-                System.out.println(e.toString());
-            }
-        } catch (Exception e) {
-            save(stringToBeSaved, absolutePath);
+    public void save(String stringToBeSaved, String absolutePath) throws Exception {
+        if (!fileName.exists()) {
+            fileName.createNewFile();
+        } else {
+            throw new Exception("The file with the name " + fileName + " already exists");
         }
     }
 
     /**
      * Prepares a file for saving (specifies the path and name
      */
-    public void prepareFileForSaving() throws IOException {
+    public void prepareFileForSaving() throws Exception {
         String stringToBeSaved = "String to be saved";
         fileName = new File("fileToBeCreated.txt");
         String absPath = fileName.getAbsolutePath();
         save(stringToBeSaved, absPath);
+        writeToFile(stringToBeSaved, absPath);
     }
 
     /**
      * Writes a string to the new file
      */
     public void writeToFile(String stringToBeWrittenToFile, String path) throws IOException {
-        String line;
         FileWriter writer = null;
-        BufferedReader reader = new BufferedReader(new FileReader(fileName));
         try {
-            writer = new FileWriter(fileName);
+            writer = new FileWriter(path);
             writer.write(stringToBeWrittenToFile);
         } finally {
             if (writer != null) {
                 writer.close();
             }
-        }
-        if ((line = reader.readLine()) == null) {
-            throw new IOException("Nothing has been written to the file");
-        } else {
-            System.out.println("The following string has been written to the file: " + line);
-            reader.close();
         }
     }
 }

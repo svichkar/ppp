c227881b74b46d32fb0fd9bdcde7cca1b036d70b
Zinovyi Dmitro <liberal4ik@gmail.com>
2016-02-08 17:39:44 +0200

    add 09_threads task

---
+++ 09_threads/com/nixsolutions/CountAndTimerTask.java
@@ -0,0 +1,60 @@
+package com.nixsolutions;
+
+/**
+ * Describe task: "Разработать приложение, в котором главный поток каждые 100ms инкрементирует
+ * счетчик от 1 до 1000, а 3 потока стартуют при достижении счеткика 100, 300 и 500 соответственно
+ * - каждую секунду выводят в консоль сообщение о своей работе"
+ *
+ * @author Dmitry Zinovyi
+ */
+public class CountAndTimerTask {
+
+    private static int count = 1;
+    private static final int CAPACITY = 1000;
+
+    public static void main(String[] argc) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (count != CAPACITY) {
+                    if (count == 100) {
+                        new SomeWorker("Developer").start();
+                    } else if (count == 300) {
+                        new SomeWorker("QA").start();
+                    } else if (count == 500) {
+                        new SomeWorker("BA").start();
+                    }
+                    ++count;
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+
+    static class SomeWorker extends Thread {
+
+        private String name = "Unknown name";
+
+        SomeWorker(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public void run() {
+            while (count != CAPACITY) {
+                System.out.println("I'm " + name + ". I'm still working...");
+                try {
+                    this.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
---
+++ 09_threads/com/nixsolutions/ProducerWithTwoConsumer.java
@@ -0,0 +1,65 @@
+package com.nixsolutions;
+
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Describe task: "Разработать приложение, где producer помещает в очередь 100 случайных чисел,
+ * а 2 consumer’а ждут появления в очереди чисел - один четных, другой нечетных - забирают их и выводят в консоль"
+ *
+ * @author Dmitry Zinovyi
+ */
+public class ProducerWithTwoConsumer {
+
+    private static final int CAPACITY = 100;
+    private static AtomicInteger count = new AtomicInteger(0);
+    private static BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(CAPACITY);
+
+    public static void main(String[] argc) {
+        new Thread(new Producer()).start();
+        new Thread(new Consumer(true)).start();
+        new Thread(new Consumer(false)).start();
+
+    }
+
+    private static class Producer implements Runnable {
+
+        @Override
+        public void run() {
+            Random random = new Random();
+            for (int i = 0; i < CAPACITY; i++) {
+                int r = random.nextInt(10);
+                queue.add(r);
+                System.out.println("Add " + i + "th value: " + r);
+            }
+        }
+
+    }
+
+    private static class Consumer implements Runnable {
+
+        private boolean even = false;
+
+        Consumer(boolean even) {
+            this.even = even;
+        }
+
+        @Override
+        public void run() {
+            while (count.get() != CAPACITY) {
+                Integer peek = queue.peek();
+                if ((peek != null) && (peek % 2 == 0) && (even)) {
+                    System.out.println("Even consumer processed value: " + queue.poll());
+                    count.addAndGet(1);
+                } else if ((peek != null) && (peek % 2 == 1) && (!even)) {
+                    System.out.println("Not even consumer processed value: " + queue.poll());
+                    count.addAndGet(1);
+                }
+            }
+        }
+
+    }
+
+}

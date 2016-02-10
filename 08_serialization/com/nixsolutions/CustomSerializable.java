2cbed1a6c5cd567e7892d6d225a817327f9a9947
Zinovyi Dmitro <liberal4ik@gmail.com>
2016-02-10 15:45:03 +0200

    fix serialize task

--- 08_serialization/com/nixsolutions/CustomSerializable.java
+++ 08_serialization/com/nixsolutions/CustomSerializable.java
@@ -1,6 +1,8 @@
 package com.nixsolutions;
 
 import java.io.*;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 public class CustomSerializable {
 
@@ -8,8 +10,11 @@ public class CustomSerializable {
         if (!(obj instanceof java.io.Serializable)) {
             throw new NotSerializableException("Class " + obj.getClass() + " not implements Serializable");
         }
-        try (ByteArrayOutputStream b = new ByteArrayOutputStream(); ObjectOutput out = new ObjectOutputStream(b)) {
+        try (ByteArrayOutputStream b = new ByteArrayOutputStream(); GZIPOutputStream gzip = new GZIPOutputStream(b);
+             ObjectOutputStream out = new ObjectOutputStream(gzip)) {
             out.writeObject(obj);
+            out.flush();
+            gzip.finish();
             return b.toByteArray();
         } catch (IOException e) {
             throw e;
@@ -17,7 +22,7 @@ public class CustomSerializable {
     }
 
     public static <T> T doDeserialize(byte[] byteArray) throws IOException, ClassNotFoundException {
-        try (ByteArrayInputStream b = new ByteArrayInputStream(byteArray); ObjectInput out = new ObjectInputStream(b)) {
+        try (ObjectInputStream out = new ObjectInputStream(new GZIPInputStream(new ByteArrayInputStream(byteArray)))) {
             return (T) out.readObject();
         } catch (IOException e) {
             throw e;

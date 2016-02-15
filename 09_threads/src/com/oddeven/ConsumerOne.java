a92875fa207cd0842570d45f6faea18bac3b6a94
Valeriy Pantiukhin <107pavalvl@gmail.com>
2016-02-15 18:18:20 +0200

    Odd/even: another commit

--- 09_threads/src/com/oddeven/ConsumerOne.java
+++ 09_threads/src/com/oddeven/ConsumerOne.java
@@ -18,26 +18,27 @@ public class ConsumerOne implements Runnable {
     public void run() {
         while (true) {
             synchronized (queue) {
-                while (queue.isEmpty())
+                while (queue.isEmpty()) {
                     try {
                         queue.wait();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                Iterator iterator = queue.iterator();
-                if (iterator.hasNext()) {
-                    num = (Integer) iterator.next();
-                }
-                if ((num % 2) != 0) {
-                    System.out.println("Thread one has removed an odd number: " + num);
-                    queue.remove();
-                    try {
-                        Thread.sleep(500);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+                    Iterator iterator = queue.iterator();
+                    if (iterator.hasNext()) {
+                        num = (Integer) iterator.next();
                     }
+                    if ((num % 2) != 0) {
+                        System.out.println("Thread one has removed an odd number: " + num);
+                        queue.remove();
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    queue.notifyAll();
                 }
-                queue.notifyAll();
             }
         }
     }

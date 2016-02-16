965419b17bdb12046283d176ed2259468fe7e283
Valeriy Pantiukhin <107pavalvl@gmail.com>
2016-02-16 16:09:54 +0200

    Threads: attempt two part one

--- 09_threads/src/com/fourthreads2/FourThreads.java
+++ 09_threads/src/com/fourthreads2/FourThreads.java
@@ -4,10 +4,10 @@ package com.fourthreads2;
  * Created by pantiukhin on 2/11/2016.
  */
 public class FourThreads {
-    private static int counter = 1;
-    private Thread thrOne = new Thread(new ThreadOne());
-    private Thread thrTwo = new Thread(new ThreadTwo());
-    private Thread thrThree = new Thread(new ThreadThree());
+    private int counter = 1;
+    private Thread thrOne = new Thread(new ThreadToRun(), "one");
+    private Thread thrTwo = new Thread(new ThreadToRun(), "two");
+    private Thread thrThree = new Thread(new ThreadToRun(), "three");
 
     public static void main(String[] args) {
         FourThreads fourThreads = new FourThreads();
@@ -19,12 +19,15 @@ public class FourThreads {
             try {
                 counter++;
                 if (counter == 100) {
+                    System.out.println(counter);
                     thrOne.start();
                 }
                 if (counter == 300) {
+                    System.out.println(counter);
                     thrTwo.start();
                 }
                 if (counter == 500) {
+                    System.out.println(counter);
                     thrThree.start();
                 }
                 Thread.sleep(100);
@@ -35,38 +38,17 @@ public class FourThreads {
         System.out.println(counter);
     }
 
-    private static class ThreadOne implements Runnable {
-
+    private class ThreadToRun implements Runnable {
         @Override
         public void run() {
-            runThread("one");
-        }
-    }
-
-    private static class ThreadTwo implements Runnable {
-
-        @Override
-        public void run() {
-            runThread("two");
-        }
-    }
-
-    private static class ThreadThree implements Runnable {
-
-        @Override
-        public void run() {
-            runThread("three");
-        }
-    }
-
-    public static void runThread(String threadNumber) {
-        try {
-            while (counter < 1000) {
-                System.out.println("I am thread " + threadNumber + " I am working now");
-                Thread.sleep(1000);
+            try {
+                while (counter < 1000) {
+                    System.out.println("I am thread " + Thread.currentThread().getName() + ". I am working now");
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
             }
-        } catch (InterruptedException ex) {
-            ex.printStackTrace();
         }
     }
 }

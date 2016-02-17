a548cd8f457630a5c53031d17db89576b1558964
Zinovyi Dmitro <liberal4ik@gmail.com>
2016-02-17 13:34:08 +0200

    full answer 07_reflections

--- 07_reflections/com/nixsolutions/loader/CustomClassLoader.java
+++ 07_reflections/com/nixsolutions/loader/CustomClassLoader.java
@@ -2,35 +2,26 @@ package com.nixsolutions.loader;
 
 import java.io.*;
 import java.nio.file.Files;
-import java.util.HashMap;
-import java.util.Map;
 
 public class CustomClassLoader extends ClassLoader implements PathClassLoader {
 
     private String pathToClass;
-    //private Map<String, Class> cache = new HashMap<>();
+
+    public CustomClassLoader(String pathToClass) {
+        setPath(pathToClass);
+    }
 
     @Override
     protected Class findClass(String className) throws ClassNotFoundException {
         try {
-            //Class<?> ret = cache.get(className);
-            Class<?> ret = null;
-            //check hui znaet che
-/*            try {
-                if (ret == null) {
-                    ret = super.findSystemClass(className);
-                }
-            } catch (ClassNotFoundException e) {
-                System.out.println(e);
-            }*/
-            if (ret == null) {
-                byte b[] = loadClassData(className);
-                ret = defineClass(className, b, 0, b.length);
-                //cache.put(className, ret);
-            }
-            return ret;
+            byte b[] = loadClassData(className);
+            return defineClass(className, b, 0, b.length);
         } catch (IOException e) {
-            throw new ClassNotFoundException(className);
+            /*
+            In my opinion, it's need because if we catch IOException we don't know why happens this
+            We redirect to super class (ClassLoader) attempt to find and to load class if need
+             */
+            return super.findSystemClass(className);
         }
     }
 
--- 07_reflections/com/nixsolutions/loader/TestLoader.java
+++ 07_reflections/com/nixsolutions/loader/TestLoader.java
@@ -5,32 +5,31 @@ import java.lang.reflect.Method;
 public class TestLoader {
 
     public static void main(String[] argc) {
-        String path = "D:\\";
-        String binaryClassName = "com.nixsolutions.WorkWithString";
+        String path = "D:\\IdeaProjects\\javappp\\02_oop";
+        String binaryClassName = "com.nixsolutions.Square";
 
-        //firstly class loaded and save in local cache ccl's object
-        CustomClassLoader ccl = new CustomClassLoader();
-        ccl.setPath(path);
-        Class uploadClass = null;
+        CustomClassLoader ccl = new CustomClassLoader(path);
         try {
-            uploadClass = ccl.loadClass(binaryClassName);
+            //firstly class loaded
+            Class uploadClass = ccl.loadClass(binaryClassName);
+            System.out.println("define class\n");
+            System.out.println(uploadClass.getName());
+            for (Method m : uploadClass.getMethods()) {
+                System.out.println(m.getName());
+            }
+            System.out.println("\n\n");
+
+            //secondly class return from cache
+            Class uploadClass2 = ccl.loadClass(binaryClassName);
+            System.out.println("return from cache\n");
+            System.out.println(uploadClass2.getName());
+            for (Method m : uploadClass.getMethods()) {
+                System.out.println(m.getName());
+            }
+
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        System.out.println(uploadClass.getName());
-        for (Method m : uploadClass.getMethods()) {
-            System.out.println(m.getName());
-        }
-
-        //secondly class loader return cached Class
-        Class uploadClass2 = null;
-        try {
-            uploadClass2 = ccl.loadClass(binaryClassName);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        System.out.println(uploadClass2.getName());
-
     }
 
 }

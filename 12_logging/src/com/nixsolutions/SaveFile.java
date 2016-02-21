c6723985fdb17e38cdc55bdb05ec0746d634cc84
sobolenko <alexey.sobolenko@gmail.com>
2016-02-21 21:54:50 +0200

    Generis and Logging

--- 12_logging/src/com/nixsolutions/SaveFile.java
+++ 12_logging/src/com/nixsolutions/SaveFile.java
@@ -16,16 +16,24 @@ public class SaveFile implements Save {
     public void save(String data, String fileName) {
         Logger LOG = LogManager.getLogger();
         File outputFile = new File(fileName);
-        LOG.info("set output file: "+outputFile);
+        LOG.info("set output file: " + outputFile);
         outputFile.getParentFile().mkdirs();
+        LOG.info("create all dirs structure, according paths " + outputFile.getParentFile());
         FileWriter fwriter = null;
+        LOG.info("fwriter variable initialize");
         try {
+            LOG.info("check if exists", outputFile.exists());
             if (!outputFile.exists()) {
+                LOG.info(outputFile + " not exists");
                 fwriter = new FileWriter(outputFile);
+                LOG.info("create new FileWriter for file '" + outputFile + "'");
                 fwriter.write(data);
+                LOG.info("write data to file:\n " + data);
             } else {
+                LOG.info(outputFile + " exists");
                 throw LOG.throwing(new CustomException("File already exists"));
             }
+            LOG.info("close all streams");
             fwriter.flush();
             fwriter.close();
         } catch (IOException io) {

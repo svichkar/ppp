93f213bdd9ebb940f12bcf5f99a837aed09acae9
Valeriy Pantiukhin <107pavalvl@gmail.com>
2016-02-25 17:36:00 +0200

    Small fix

--- 10_generics/src/com/converter/IntegerArrayStringConverter.java
+++ 10_generics/src/com/converter/IntegerArrayStringConverter.java
@@ -1,23 +1,19 @@
 package com.converter;
 
-import java.util.Iterator;
 
 /**
  * Created by val on 2/21/2016.
  */
 
 public class IntegerArrayStringConverter<I, T> implements Converter<I, T> {
-    private StringBuilder sb = new StringBuilder();
-
     @Override
     public T get(I i) {
-        T s = (T) "";
+        StringBuilder sb = new StringBuilder();
         I[] arr = (I[]) i;
         for (int y = 0; y < arr.length; y++) {
             sb.append(arr[y].toString() + " ");
-            s = (T) sb.toString();
         }
-        return s;
+        return (T) sb.toString();
     }
 }
 

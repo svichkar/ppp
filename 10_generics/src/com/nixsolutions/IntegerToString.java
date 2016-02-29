b15e98a25417986329e1f1e65135842d0ff61b80
Alexey Sobolenko <a.sobolenko@nixsolutions.com>
2016-02-29 10:08:35 +0200

    fix generics

--- 10_generics/src/com/nixsolutions/IntegerToString.java
+++ 10_generics/src/com/nixsolutions/IntegerToString.java
@@ -1,12 +1,14 @@
 package com.nixsolutions;
 
-import java.util.Arrays;
-
 /**
  * Created by sobolenko on 2/18/2016.
  */
-public class IntegerToString implements Converter<Integer[],String> {
+public class IntegerToString implements Converter<Integer[], String> {
     public String get(Integer[] array) {
-        return Arrays.toString(array);
+        String result = "";
+        for (Integer value : array) {
+            result += value.toString() + " ";
+        }
+        return result;
     }
 }
